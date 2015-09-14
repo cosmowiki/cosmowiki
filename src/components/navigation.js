c2d11fa12f0db3022d2a5938300a380023d0ceda
Wolfram Kriesing <wk@uxebu.com>
2015-09-14 13:29:27 +0200

    The real navigation.

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,7 +2,105 @@ export default class Navigation {
   
   render() {
     return (
-      <div>iam navigation</div>
+      
+      <div id="wrapper" className="pure-g">
+        <header className="pure-u-2-3">
+          <div id="branding">
+            <a href="index.php" title="CosmoWiki.de" rel="home"><img src="http://cosmowiki.de/img/cw_header.jpg" width="500" height="75" alt="CosmoWiki.de" /></a>
+          </div>
+          <div id="social">
+            <ul>
+              <li className="social-button facebook center">
+                <a href="https://www.facebook.com/pages/CosmoWikide/120878727989418" target="_blank" title="CosmoWiki.de bei Facebook"></a>
+              </li>
+              <li className="social-button googleplus center">
+                <a href="https://plus.google.com/u/0/+CosmoWikide/posts" target="_blank" title="CosmoWiki.de bei Google Plus"></a>
+              </li>
+              <li className="social-button twitter center">
+                <a href="http://www.twitter.com/CosmoWiki_de" target="_blank" title="CosmoWiki.de bei Twitter"></a>
+              </li>
+              <li className="social-button github center">
+                <a href="https://github.com/cosmowiki/cosmowikide" target="_blank" title="CosmoWiki.de bei Github"></a>
+              </li>
+              <li className="social-button info center">
+                <a href="about.php" target="_self" title="&Uuml;ber CosmoWiki.de"></a>
+              </li>
+            </ul>
+          </div>
+          <nav className="pure-u-1 pure-menu pure-menu-horizontal">
+            <ul id="menu" className="pure-menu-list">
+              <li className="pure-menu-item">
+                <a className="pure-menu-link menu-item chronicle" href="chronicle.php">Chronik</a>
+              </li>
+              <li className="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
+                <a className="pure-menu-link menu-item persons" href="persons.php">Personen</a>
+                <ul className="pure-menu-children sub-menu persons">
+                  <li className="pure-menu-item">
+                    <a className="pure-menu-link sub-menu-item persons" href="astronomers.php">Astronomen</a>
+                  </li>
+                  <li className="pure-menu-item">
+                    <a className="pure-menu-link sub-menu-item persons" href="astronauts.php">Raumfahrer</a>
+                  </li>
+                </ul>
+              </li>
+              <li className="pure-menu-item">
+                <a className="pure-menu-link menu-item places" href="places.php">Orte</a>
+              </li>
+              <li className="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
+                <a className="pure-menu-link menu-item objects" href="objects.php">Objekte</a>
+                <ul className="pure-menu-children sub-menu objects">
+                  <li className="pure-menu-item">
+                    <a className="pure-menu-link sub-menu-item objects" href="solsys.php">Sonnensystem</a>
+                  </li>
+                  <li className="pure-menu-item">
+                    <a className="pure-menu-link sub-menu-item objects" href="constellations.php">Sternbilder</a>
+                  </li>
+                  <li className="pure-menu-item">
+                    <a className="pure-menu-link sub-menu-item objects" href="stars.php">Sterne</a>
+                  </li>
+                  <li className="pure-menu-item">
+                    <a className="pure-menu-link sub-menu-item objects" href="stations.php">Raumstationen</a>
+                  </li>
+                </ul>
+              </li>
+              <li className="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
+                <a className="pure-menu-link menu-item astronomy" href="astronomy.php">Astronomie</a>
+                <ul className="pure-menu-children sub-menu astronomy">
+                  <li className="pure-menu-item">
+                    <a className="pure-menu-link sub-menu-item astronomy" href="astronomers.php">Astronomen</a>
+                  </li>
+                  <li className="pure-menu-item">
+                    <a className="pure-menu-link sub-menu-item astronomy" href="solsys.php">Sonnensystem</a>
+                  </li>
+                  <li className="pure-menu-item">
+                    <a className="pure-menu-link sub-menu-item astronomy" href="constellations.php">Sternbilder</a>
+                  </li>
+                  <li className="pure-menu-item">
+                    <a className="pure-menu-link sub-menu-item astronomy" href="stars.php">Sterne</a>
+                  </li>
+                </ul>
+              </li>
+              <li className="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
+                <a className="pure-menu-link menu-item spaceflight" href="spaceflight.php">Raumfahrt</a>
+                <ul className="pure-menu-children sub-menu spaceflight">
+                  <li className="pure-menu-item">
+                    <a className="pure-menu-link sub-menu-item spaceflight" href="astronauts.php">Raumfahrer</a>
+                  </li>
+                  <li className="pure-menu-item">
+                    <a className="pure-menu-link sub-menu-item spaceflight" href="missions.php">Missionen</a>
+                  </li>
+                  <li className="pure-menu-item">
+                    <a className="pure-menu-link sub-menu-item spaceflight" href="stations.php">Raumstationen</a>
+                  </li>
+                </ul>
+              </li>
+              <li className="pure-menu-item">
+                <a className="pure-menu-link menu-item register" href="register.php">Register</a>
+              </li>
+            </ul>
+          </nav>
+        </header>      
+      </div>         
     )
   }
   
