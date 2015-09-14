f65b97d0633c01175629ef8d973803366b1c29fd
Wolfram Kriesing <wk@uxebu.com>
2015-09-14 23:18:01 +0200

    Script for downloading all data from the spreadsheet into local json files.

---
+++ data/update-data.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "downloading data"
+
+FILENAME=data/chronicle.json
+gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 0
+echo "`du -h $FILENAME` loaded"
+
+FILENAME=data/chronicle.json
+gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 1
+echo "`du -h $FILENAME` loaded"
+
+FILENAME=data/places.json
+gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 2
+echo "`du -h $FILENAME` loaded"
+
+FILENAME=data/missions.json
+gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 3
+echo "`du -h $FILENAME` loaded"
+
+FILENAME=data/stars.json
+gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 4
+echo "`du -h $FILENAME` loaded"
+
+FILENAME=data/constellations.json
+gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 5
+echo "`du -h $FILENAME` loaded"
+
+FILENAME=data/spacestations.json
+gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 6
+echo "`du -h $FILENAME` loaded"
