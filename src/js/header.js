331c207f16c68f9b11aae5a0bedb657f1801f642
H. Porsch <holly@Holgers-MBP.fritz.box>
2015-09-14 13:03:40 +0200

    added header.js

---
+++ src/js/header.js
@@ -0,0 +1,135 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+"http://www.w3.org/TR/html4/strict.dtd"
+>
+<!DOCTYPE html>
+<html lang="de-DE">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>CosmoWiki.de | die Online-Enzyklop&auml;die &uuml;ber Astronomie und Raumfahrt</title>
+<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+<!--[if lte IE 8]>
+<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/grids-responsive-old-ie-min.css">
+<![endif]-->
+<!--[if gt IE 8]><!-->
+<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/grids-responsive-min.css">
+<!--<![endif]-->
+<link rel="stylesheet" type="text/css" media="all" href="css/cw.css" />
+<link rel="stylesheet" type="text/css" href="shariff/build/shariff.complete.css" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+<script type='text/javascript' src="js/cw.js"></script>
+<script type='text/javascript' src="js/menus.js"></script>
+<script type='text/javascript' src='https://platform.twitter.com/widgets.js?ver=4.0.1'></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/tablesorter/jquery-latest.js"></script> 
+<script type="text/javascript" src="js/tablesorter/jquery.tablesorter.js"></script>
+<link rel='canonical' href='http://de.cosmowiki.de/' />
+<link rel='shortlink' href='http://de.cosmowiki.de/' />
+</head>
+<body>
+<div id="wrapper" class="pure-g">
+	<header class="pure-u-2-3">
+		<div id="branding">
+			<a href="index.php" title="CosmoWiki.de" rel="home"><img src="http://cosmowiki.de/img/cw_header.jpg" width="500" height="75" alt="CosmoWiki.de" /></a>
+		</div>
+		<div id="social">
+			<ul>
+				<li class="social-button facebook center">
+					<a href="https://www.facebook.com/pages/CosmoWikide/120878727989418" target="_blank" title="CosmoWiki.de bei Facebook"></a>
+				</li>
+				<li class="social-button googleplus center">
+					<a href="https://plus.google.com/u/0/+CosmoWikide/posts" target="_blank" title="CosmoWiki.de bei Google Plus"></a>
+				</li>
+				<li class="social-button twitter center">
+					<a href="http://www.twitter.com/CosmoWiki_de" target="_blank" title="CosmoWiki.de bei Twitter"></a>
+				</li>
+				<li class="social-button github center">
+					<a href="https://github.com/cosmowiki/cosmowikide" target="_blank" title="CosmoWiki.de bei Github"></a>
+				</li>
+				<li class="social-button info center">
+					<a href="about.php" target="_self" title="&Uuml;ber CosmoWiki.de"></a>
+				</li>
+				<!--<li class="social-button search center">
+					<a href="#" onclick="toggle('searchDiv')" title="CosmoWiki.de durchsuchen"></a>
+				</li>-->
+			</ul>
+		</div>
+		<nav class="pure-u-1 pure-menu pure-menu-horizontal">
+			<ul id="menu" class="pure-menu-list">
+				<li class="pure-menu-item">
+					<a class="pure-menu-link menu-item chronicle" href="chronicle.php">Chronik</a>
+				</li>
+				<li class="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
+					<a class="pure-menu-link menu-item persons" href="persons.php">Personen</a>
+					<ul class="pure-menu-children sub-menu persons">
+						<li class="pure-menu-item">
+							<a class="pure-menu-link sub-menu-item persons" href="astronomers.php">Astronomen</a>
+						</li>
+						<li class="pure-menu-item">
+							<a class="pure-menu-link sub-menu-item persons" href="astronauts.php">Raumfahrer</a>
+						</li>
+					</ul>
+				</li>
+				<li class="pure-menu-item">
+					<a class="pure-menu-link menu-item places" href="places.php">Orte</a>
+				</li>
+				<li class="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
+					<a class="pure-menu-link menu-item objects" href="objects.php">Objekte</a>
+					<ul class="pure-menu-children sub-menu objects">
+						<li class="pure-menu-item">
+							<a class="pure-menu-link sub-menu-item objects" href="solsys.php">Sonnensystem</a>
+						</li>
+						<li class="pure-menu-item">
+							<a class="pure-menu-link sub-menu-item objects" href="constellations.php">Sternbilder</a>
+						</li>
+						<li class="pure-menu-item">
+							<a class="pure-menu-link sub-menu-item objects" href="stars.php">Sterne</a>
+						</li>
+						<li class="pure-menu-item">
+							<a class="pure-menu-link sub-menu-item objects" href="stations.php">Raumstationen</a>
+						</li>
+					</ul>
+				</li>
+				<li class="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
+					<a class="pure-menu-link menu-item astronomy" href="astronomy.php">Astronomie</a>
+					<ul class="pure-menu-children sub-menu astronomy">
+						<li class="pure-menu-item">
+							<a class="pure-menu-link sub-menu-item astronomy" href="astronomers.php">Astronomen</a>
+						</li>
+						<li class="pure-menu-item">
+							<a class="pure-menu-link sub-menu-item astronomy" href="solsys.php">Sonnensystem</a>
+						</li>
+						<li class="pure-menu-item">
+							<a class="pure-menu-link sub-menu-item astronomy" href="constellations.php">Sternbilder</a>
+						</li>
+						<li class="pure-menu-item">
+							<a class="pure-menu-link sub-menu-item astronomy" href="stars.php">Sterne</a>
+						</li>
+					</ul>
+				</li>
+				<li class="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
+					<a class="pure-menu-link menu-item spaceflight" href="spaceflight.php">Raumfahrt</a>
+					<ul class="pure-menu-children sub-menu spaceflight">
+						<li class="pure-menu-item">
+							<a class="pure-menu-link sub-menu-item spaceflight" href="astronauts.php">Raumfahrer</a>
+						</li>
+						<li class="pure-menu-item">
+							<a class="pure-menu-link sub-menu-item spaceflight" href="missions.php">Missionen</a>
+						</li>
+						<li class="pure-menu-item">
+							<a class="pure-menu-link sub-menu-item spaceflight" href="stations.php">Raumstationen</a>
+						</li>
+					</ul>
+				</li>
+				<li class="pure-menu-item">
+					<a class="pure-menu-link menu-item register" href="register.php">Register</a>
+				</li>
+			</ul>
+		</nav>
+		<!--<div id="searchDiv" class="searchDiv right hidden">
+			<form id="searchForm" role="search" method="get" action="">
+				<input id="searchInput" type="text" name="s" value="" placeholder="in CosmoWiki suchen" required>
+				<input id="searchSubmit" type="button" value="&#xf002;">
+			</form>
+		</div>-->
+	</header>
