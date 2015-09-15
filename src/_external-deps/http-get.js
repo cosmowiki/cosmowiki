5ea76fb26fff3e789e3641c388f1f9124756d5a1
Wolfram Kriesing <wk@uxebu.com>
2015-09-15 11:09:59 +0200

    External lib for loading json file(s).

---
+++ src/_external-deps/http-get.js
@@ -0,0 +1,16 @@
+import http from 'http';
+import url from 'url';
+
+export function loadRemoteFile(fileUrl, onLoaded) {
+  var data = '';
+  var options = url.parse(fileUrl);
+  options.withCredentials = false;
+  var request = http.request(options, function(res) {
+    res.on('data', function(chunk) { data += chunk; });
+    res.on('end', function() {
+      onLoaded(null, data);
+    });
+  });
+  request.on('error', function(e) { onLoaded(e); });
+  request.end();
+}
