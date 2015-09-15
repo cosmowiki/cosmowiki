69841403553fe4ca0d150e8e0cb7f8b2fb407227
Wolfram Kriesing <wk@uxebu.com>
2015-09-15 11:09:46 +0200

    The chronicle component, incl its controller.

--- src/components/chronicle.js
+++ src/components/chronicle.js
@@ -1,16 +1,21 @@
-export default class Chronicle {
+import Notes from './notes';
+
+export default class ChronicleComponent {
 
   render() {
-
+    
+    let items = this.props.items;
+    
     return (
-      <main role="main" class="pure-u-2-3">
-        <div id="featured" class="chronicle center">
+      <main role="main" className="pure-u-2-3">
+        <div id="featured" className="chronicle center">
           <h1>Chronik</h1>
           <p>Meilensteine der Astronomie und Raumfahrt</p>
         </div>
         <div id="dataTable">
           <div id="timeline">
             <div id="timelineHeader"></div>
+            {items.map(item => <ItemComponent item={item} />)}
             <div id="timelineFooter"></div>
           </div>
         </div>
@@ -21,32 +26,18 @@ export default class Chronicle {
     )
   }
 }
-//<?php
-//$link = mysqli_connect("localhost", "d01d6726", "tAdbJYXJhykvY7bR", "d01d6726");
-//mysqli_set_charset($link, "utf8");
-//// check connection
-//if (mysqli_connect_errno()) {
-//	printf("Keine Verbindung zur Datenbank: %s\n", mysqli_connect_error());
-//	exit();
-//}
-//$query = "SELECT chronicleY, chronicleM, chronicleD, chronicleLoc,
-//chronicleEvent, chronicleLink, chronicleLat, chronicleLong, type
-//FROM chronicle";
-//if ($result = mysqli_query($link, $query)) {
-///* fetch object array */
-//	while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-//		$date = array($row[chronicleD], $row[chronicleM], $row[chronicleY]);
-//		$date = implode('.', array_filter($date));
-//		echo "<div class=\"timelineRow\">
-//		<div class=\"timelineDate right\">$date</div>
-//		<div class=\"timelinePlace\">$row[chronicleLoc]</div>
-//		<div class=\"timelineEvent\"><a href=$row[chronicleLink]>$row[chronicleEvent]</a></div>
-//		</div>";
-//	}
-//// free result set
-//	mysqli_free_result($result);
-//}
-//// close connection
-//mysqli_close($link);
-//?>
-//
+
+class ItemComponent {
+  
+  render() {
+    const item = this.props.item;
+    return (
+      <div className="timelineRow">
+        <div className="timelineDate right">{item.readableDate}</div>
+        <div className="timelinePlace">{item.location}</div>
+        <div className="timelineEvent"><a href={item.wikipediaUrl}>{item.event}</a></div>
+      </div>
+    )
+  }
+  
+}
