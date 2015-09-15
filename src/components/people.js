a7da80ad274389bdc29c8ec33334ad1534296797
Wolfram Kriesing <wk@uxebu.com>
2015-09-15 13:15:41 +0200

    First draw for the people component.

---
+++ src/components/people.js
@@ -0,0 +1,240 @@
+import Notes from './notes';
+
+export default class PeopleComponent {
+  
+  render() {
+    
+    const groupedPeople = this.props.groupedPeople;
+    
+    return (
+      <main role="main" className="pure-u-2-3">
+        <div id="featured" className="persons center">
+          <h1>Personen</h1>
+          <p>Entdecker, Pioniere, Wissenschaftler</p>
+        </div>
+        
+        <div id="filter" className="person">
+          <form id="filterCountry">
+            <span>filtern nach Land:</span>
+          </form>
+          <form id="filterProfession">
+            <span>filtern nach Beruf:</span>
+          </form>
+        </div>
+        
+        <div id="letterLinks" className="person center">
+          <a href="#A">A</a> -
+          <a href="#B">B</a> -
+          <a href="#C">C</a> -
+          <a href="#D">D</a> -
+          <a href="#E">E</a> -
+          <a href="#F">F</a> -
+          <a href="#G">G</a> -
+          <a href="#H">H</a> -
+          <a href="#I">I</a> -
+          <a href="#J">J</a> -
+          <a href="#K">K</a> -
+          <a href="#L">L</a> -
+          <a href="#M">M</a> -
+          <a href="#N">N</a> -
+          <a href="#O">O</a> -
+          <a href="#P">P</a> -
+          <a href="#Q">Q</a> -
+          <a href="#R">R</a> -
+          <a href="#S">S</a> -
+          <a href="#T">T</a> -
+          <a href="#U">U</a> -
+          <a href="#V">V</a> -
+          <a href="#W">W</a> -
+          <a href="#X">X</a> -
+          <a href="#Y">Y</a> -
+          <a href="#Z">Z</a>
+        </div>
+        
+        <div id="dataTable" className="person">
+          <div id="personTable">
+            <div className="firstLetterRow">
+            </div>
+            {groupedPeople.map(group => <PersonGroupComponent group={group} />)}
+          </div>
+        </div>
+        <div id="notes">
+          <Notes />
+        </div>
+      </main>    
+    );
+  }
+  
+}
+
+class PersonGroupComponent {
+  
+  render() {
+    const group = this.props.group;
+    const groupKey = group.key;
+    const people = group.people;
+    
+    return (
+      <div className="pure-u-1">
+        <h1>{groupKey}</h1>
+        {people.map(person => <PersonComponent person={person} />)}
+      </div>      
+    );
+  }
+  
+}
+
+class PersonComponent {
+  
+  render() {
+    const person = this.props.person;
+    //<a onMouseOver={`toggleIn('${person.name}')`} onMouseOut={`toggleOut('${person.name}')`} href={person.wikipediaUrl}>{person.name}</a>
+    return (
+      <div className="personRow pure-u-1 pure-u-md-1-3">
+        <div className="personItem">
+          <a href={person.wikipediaUrl}>{person.name}</a>
+        </div>
+        <div id={person.name} className="hidden personInfoBox">
+          <div className="personProfession">$row[personProf]</div>
+          <div className="personLife">$born   $died</div>
+          <div className="personCountry">$row[personCountry]</div>
+          <div className="personInfo justify">$row[personDescript]</div>
+        </div>
+      </div>
+    );
+    
+//		$name = array($row[personName], $row[personFirstName]);
+//		$name = implode(', ', array_filter($name));
+//		// merge the date array elements yyyy mm dd to a dd.mm.yyyy string
+//		$born = array($row[personBornD], $row[personBornM], $row[personBornY]);
+//		$died = array($row[personDiedD], $row[personDiedM], $row[personDiedY]);
+//		$born = implode('.', array_filter($born));
+//		if (!empty($born)) {
+//			$born = html_entity_decode('&lowast;').' '.$born;
+//		}
+//		$died = implode('.', array_filter($died));
+//		if (!empty($died)) {
+//			$died = html_entity_decode('&dagger;').' '.$died;
+//		}
+//		echo "<div class=\"personRow\">
+//				<div class=\"personItem\">
+//					<a onMouseOver=\"toggleIn('$name')\" onMouseOut=\"toggleOut('$name')\" href=$row[personLink]>$name</a>
+//				</div>
+//				<div id=\"$name\" class=\"hidden personInfoBox\">
+//					<div class=\"personProfession\">$row[personProf]</div>
+//					<div class=\"personLife\">$born   $died</div>
+//					<div class=\"personCountry\">$row[personCountry]</div>
+//					<div class=\"personInfo justify\">$row[personDescript]</div>
+//				</div>
+//			</div>";
+  }
+  
+}
+
+//<?php include("header.php")?>
+//	<main role="main" class="pure-u-2-3">
+//		<div id="featured" class="persons center">
+//			<h1>Personen</h1>
+//			<p>Entdecker, Pioniere, Wissenschaftler</p>
+//		</div>
+//		<div id="filter" class="person">
+//			<form id="filterCountry">
+//				<span style="font-weight: bold; ">filtern nach Land:</span>
+//			</form>
+//			<form id="filterProfession">
+//				<span style="font-weight: bold; ">filtern nach Beruf:</span>
+//			</form>
+//		</div>
+//		<div id="letterLinks" class="person center">
+//			<a href="#A">A</a> -
+//			<a href="#B">B</a> -
+//			<a href="#C">C</a> -
+//			<a href="#D">D</a> -
+//			<a href="#E">E</a> -
+//			<a href="#F">F</a> -
+//			<a href="#G">G</a> -
+//			<a href="#H">H</a> -
+//			<a href="#I">I</a> -
+//			<a href="#J">J</a> -
+//			<a href="#K">K</a> -
+//			<a href="#L">L</a> -
+//			<a href="#M">M</a> -
+//			<a href="#N">N</a> -
+//			<a href="#O">O</a> -
+//			<a href="#P">P</a> -
+//			<a href="#Q">Q</a> -
+//			<a href="#R">R</a> -
+//			<a href="#S">S</a> -
+//			<a href="#T">T</a> -
+//			<a href="#U">U</a> -
+//			<a href="#V">V</a> -
+//			<a href="#W">W</a> -
+//			<a href="#X">X</a> -
+//			<a href="#Y">Y</a> -
+//			<a href="#Z">Z</a>
+//		</div>
+//		<div id="dataTable" class="person">
+//			<div id="personTable">
+//				<div class="firstLetterRow">
+//					<!--<span class="firstLetter">
+//						<a name="" href="">$letter</a> name=\'$letter\'</a> 
+//					</span>
+//					<span class="firstLetterArrow">
+//						<a href="javascript:self.scrollTo(0,0);">&uarr;</a>
+//					</span>-->
+//				</div>
+//
+//<?php
+//$link = mysqli_connect("localhost", "d01d6726", "tAdbJYXJhykvY7bR", "d01d6726");
+//mysqli_set_charset($link, "utf8");
+//// check connection
+//if (mysqli_connect_errno()) {
+//	printf("Keine Verbindung zur Datenbank: %s\n", mysqli_connect_error());
+//	exit();
+//}
+//$query = "SELECT personName, personFirstName, personProf, personCountry,
+//personBornY, personBornM, personBornD, personDiedY, personDiedM, personDiedD,
+//personDescript, personLink, type FROM persons ORDER by personName";
+//if ($result = mysqli_query($link, $query)) {
+//// fetch object array
+//	while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+//		// merge the name array elements to a name string
+//		$name = array($row[personName], $row[personFirstName]);
+//		$name = implode(', ', array_filter($name));
+//		// merge the date array elements yyyy mm dd to a dd.mm.yyyy string
+//		$born = array($row[personBornD], $row[personBornM], $row[personBornY]);
+//		$died = array($row[personDiedD], $row[personDiedM], $row[personDiedY]);
+//		$born = implode('.', array_filter($born));
+//		if (!empty($born)) {
+//			$born = html_entity_decode('&lowast;').' '.$born;
+//		}
+//		$died = implode('.', array_filter($died));
+//		if (!empty($died)) {
+//			$died = html_entity_decode('&dagger;').' '.$died;
+//		}
+//		echo "<div class=\"personRow\">
+//				<div class=\"personItem\">
+//					<a onMouseOver=\"toggleIn('$name')\" onMouseOut=\"toggleOut('$name')\" href=$row[personLink]>$name</a>
+//				</div>
+//				<div id=\"$name\" class=\"hidden personInfoBox\">
+//					<div class=\"personProfession\">$row[personProf]</div>
+//					<div class=\"personLife\">$born   $died</div>
+//					<div class=\"personCountry\">$row[personCountry]</div>
+//					<div class=\"personInfo justify\">$row[personDescript]</div>
+//				</div>
+//			</div>";
+//	}
+//// free result set
+//	mysqli_free_result($result);
+//}
+//// close connection
+//mysqli_close($link);
+//?>
+//
+//			</div>
+//		</div>
+//		<div id="notes">
+//<?php include("notes.php")?>
+//		</div>
+//	</main><!-- #main -->
+//<?php include("footer.php")?>
---
+++ src/lexicon/people.js
@@ -0,0 +1,87 @@
+import PeopleComponent from '../components/people';
+
+export default class People {
+  
+  static componentWithData(onDone) {
+    Items.loadFromJsonFile('/data/people.json', (people) => {
+      const grouped = new GroupedByFirstLetter(people);
+      const component = <PeopleComponent groupedPeople={grouped.groups} />;
+      onDone(component);
+    });    
+  }
+  
+}
+
+class GroupedByFirstLetter {
+  
+  constructor(items) {
+    this.items = items;
+  }
+  
+  groupedPeople() {
+    let grouped = {};
+    this.items
+      .map(({name}) => name[0].toUpperCase())
+      .filter(firstLetter => !grouped[firstLetter])
+      .forEach(firstLetter => grouped[firstLetter] = {
+        key: firstLetter,
+        people: []
+      });
+    
+    this.items.forEach(person => {
+      const key = person.name[0].toUpperCase();
+      grouped[key].people.push(person);
+    });
+    return grouped;
+  }
+  
+  get groups() {
+    const groupedPeople = this.groupedPeople();
+    return Object.keys(groupedPeople)
+      .map(key => groupedPeople[key]);
+  }
+  
+}
+
+
+import {loadRemoteFile} from '../_external-deps/http-get';
+class Items {
+  
+  static loadFromJsonFile(url, onDone, onError) {
+    loadRemoteFile(url, (err, data) => {
+      if (err) {
+        console.log(`Error loading data from ${url}, ${err}`);
+      } else {
+
+        const items = JSON.parse(data).map(raw => Item.fromRawData(raw));
+        onDone(items);
+      }
+    });
+  }
+  
+}
+
+class Item {
+  
+  static fromRawData(raw) {
+    //"personname": "Abbe",
+    //"personfirstname": "Cleveland",
+    //"personprof": "Astronom, Meteorologe",
+    //"personcountry": "USA",
+    //"personborny": "1838",
+    //"personbornm": "12",
+    //"personbornd": "3",
+    //"persondiedy": "1916",
+    //"persondiedm": "10",
+    //"persondiedd": "28",
+    //"persondescript": "erster Meteorologe",
+    //"personlink": "https://de.wikipedia.org/wiki/Cleveland_Abbe",
+    //"type": 1       1-astronomer 2-astronaut 3-both 0-neither
+    const item = new Item();
+    item.name = [raw.personname, raw.personfirstname].join(', ');
+    item.wikipediaUrl = raw.personlink;
+    //item.tags = raw.tags.split(',');
+    return item;
+  }
+  
+}
