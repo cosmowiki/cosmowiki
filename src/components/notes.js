3fcf305362edd923eac329f089c1b467d6c45bfe
Wolfram Kriesing <wk@uxebu.com>
2015-09-15 13:15:21 +0200

    Use className attr.

--- src/components/notes.js
+++ src/components/notes.js
@@ -3,7 +3,7 @@ export default class Notes {
   render() {
 
     return (
-      <p id="notComplete" class="center">
+      <p id="notComplete" className="center">
         Diese Liste ist nicht vollst&auml;ndig und wird st&auml;ndig erweitert.<br/>
         Sie kennen noch mehr Daten zu dieser Seite? Bereichern Sie dieses Lexikon und melden Sie uns diese bitte <a
         href="http://de.cosmowiki.de/?page_id=22" target="_self" title="Kontakt">hier</a>.
