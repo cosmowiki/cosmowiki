ec1a7bb0e0ce6960aeeee33fe2dfd9e2024c9e7c
Wolfram Kriesing <wk@uxebu.com>
2015-09-16 12:17:48 +0200

    Move the letters navigation out of the main component.

--- src/components/people.js
+++ src/components/people.js
@@ -13,34 +13,7 @@ export default class PeopleComponent {
           <p>Entdecker, Pioniere, Wissenschaftler</p>
         </div>
         
-        <div id="letterLinks" className="person center">
-          <a href="#A">A</a> -
-          <a href="#B">B</a> -
-          <a href="#C">C</a> -
-          <a href="#D">D</a> -
-          <a href="#E">E</a> -
-          <a href="#F">F</a> -
-          <a href="#G">G</a> -
-          <a href="#H">H</a> -
-          <a href="#I">I</a> -
-          <a href="#J">J</a> -
-          <a href="#K">K</a> -
-          <a href="#L">L</a> -
-          <a href="#M">M</a> -
-          <a href="#N">N</a> -
-          <a href="#O">O</a> -
-          <a href="#P">P</a> -
-          <a href="#Q">Q</a> -
-          <a href="#R">R</a> -
-          <a href="#S">S</a> -
-          <a href="#T">T</a> -
-          <a href="#U">U</a> -
-          <a href="#V">V</a> -
-          <a href="#W">W</a> -
-          <a href="#X">X</a> -
-          <a href="#Y">Y</a> -
-          <a href="#Z">Z</a>
-        </div>
+        <LetterLinks />
         
         <div id="dataTable" className="person">
           <div id="personTable">
@@ -58,6 +31,43 @@ export default class PeopleComponent {
   
 }
 
+class LetterLinks {
+  
+  render() {
+    return (
+      <div id="letterLinks" className="person center">
+        <a href="#A">A</a> -
+        <a href="#B">B</a> -
+        <a href="#C">C</a> -
+        <a href="#D">D</a> -
+        <a href="#E">E</a> -
+        <a href="#F">F</a> -
+        <a href="#G">G</a> -
+        <a href="#H">H</a> -
+        <a href="#I">I</a> -
+        <a href="#J">J</a> -
+        <a href="#K">K</a> -
+        <a href="#L">L</a> -
+        <a href="#M">M</a> -
+        <a href="#N">N</a> -
+        <a href="#O">O</a> -
+        <a href="#P">P</a> -
+        <a href="#Q">Q</a> -
+        <a href="#R">R</a> -
+        <a href="#S">S</a> -
+        <a href="#T">T</a> -
+        <a href="#U">U</a> -
+        <a href="#V">V</a> -
+        <a href="#W">W</a> -
+        <a href="#X">X</a> -
+        <a href="#Y">Y</a> -
+        <a href="#Z">Z</a>
+      </div>
+    );
+  }
+  
+}
+
 class PersonGroupComponent {
   
   render() {
@@ -67,7 +77,7 @@ class PersonGroupComponent {
     
     return (
       <div className="pure-u-1">
-        <h1>{groupKey}</h1>
+        <a name={`#/people/${groupKey}`}>{groupKey}</a><br />
         {people.map(person => <PersonComponent person={person} />)}
       </div>      
     );
