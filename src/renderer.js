bcf98b83bc023f004293f33c0276fff9b589126e
Wolfram Kriesing <wk@uxebu.com>
2015-09-25 17:16:16 +0200

    The new renderer.

---
+++ src/renderer.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import AppUrl from './appurl'
+
+let appUrl = new AppUrl();
+
+const rerender = (siteComponent) => {
+  const rendered = React.renderToString(<PageComponent siteComponent={siteComponent} appUrl={appUrl} />);
+  return rendered;
+};
+
+import {parse as parseUrl} from 'url';
+import PageComponent from './components/page';
+
+import Home from './components/home';
+import Chronicle from './lexicon/chronicle';
+import People from './lexicon/people';
+import Astronomers from './lexicon/astronomers';
+import Astronauts from './lexicon/astronauts';
+
+//window.addEventListener('hashchange', ({newURL: newUrl}) => {
+//  renderSite(newUrl);  
+//});
+
+function renderSite(path, onDone) {
+  if (path.startsWith('/chronicle')) {
+    Chronicle.componentWithData((chronicleComponent) => {
+      onDone(rerender(chronicleComponent));
+    });
+  //}
+  //if (path.startsWith('/people')) {
+  //  People.componentWithData((peopleComponent) => {
+  //    rerender(peopleComponent);
+  //  });
+  //}
+  //if (parsedUrl.hash.match(/^#\/astronomers/)) {
+  //  Astronomers.componentWithData((component) => {
+  //    rerender(component);
+  //  });
+  //}
+  //if (parsedUrl.hash.match(/^#\/astronauts/)) {
+  //  Astronauts.componentWithData((component) => {
+  //    rerender(component);
+  //  });
+  } else {
+    onDone(rerender(<Home appUrl={appUrl} />));
+  }
+}
+
+import fs from 'fs';
+import mkdirp from 'mkdirp';
+import {join as pathJoin} from 'path';
+
+function renderAndStoreSite(path) {
+  renderSite(path, renderedSite => {
+    const pathToFile = pathJoin(__dirname, '../dist', path);
+    mkdirp(pathToFile);
+    fs.writeFileSync(pathJoin(pathToFile, 'index.html'), placeInsideIndexHtml(renderedSite));
+  });
+}
+
+function placeInsideIndexHtml(content) {
+  const indexHtml = fs.readFileSync(pathJoin(__dirname, 'index.html'), 'utf8');
+  return indexHtml.replace('<div id="app"></div>', content);
+}
+
+renderAndStoreSite('/');
+renderAndStoreSite('/chronicle');
