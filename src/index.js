2cc60f7ed803cb67b321cc770fbad1adcc52aa31
Wolfram Kriesing <wk@uxebu.com>
2015-09-26 11:44:33 +0200

    Remove old code.

--- src/index.js
+++ src/index.js
@@ -1,49 +1,4 @@
 import 'babel/polyfill';
-
-//const rerender = (siteComponent) => {
-//  React.render(<Page siteComponent={siteComponent} />, document.getElementById('app'));
-//};
-//
-//import {parse as parseUrl} from 'url';
-//import Events from './lexicon/chronicle';
-//import People from './lexicon/people';
-//import Astronomers from './lexicon/astronomers';
-//import Astronauts from './lexicon/astronauts';
-//
-//window.addEventListener('hashchange', ({newURL: newUrl}) => {
-//  processUrl(newUrl);  
-//});
-//
-//function processUrl(url) {
-//  const parsedUrl = parseUrl(url);
-//  if (parsedUrl && parsedUrl.hash && parsedUrl.hash.startsWith('#/')) {
-//    if (parsedUrl.hash.match(/^#\/chronicle/)) {
-//      Events.componentWithData((chronicleComponent) => {
-//        rerender(chronicleComponent);
-//      });
-//    }
-//    if (parsedUrl.hash.match(/^#\/people/)) {
-//      People.componentWithData((peopleComponent) => {
-//        rerender(peopleComponent);
-//      });
-//    }
-//    if (parsedUrl.hash.match(/^#\/astronomers/)) {
-//      Astronomers.componentWithData((component) => {
-//        rerender(component);
-//      });
-//    }
-//    if (parsedUrl.hash.match(/^#\/astronauts/)) {
-//      Astronauts.componentWithData((component) => {
-//        rerender(component);
-//      });
-//    }
-//  } else {
-//    rerender(<Home appUrl={appUrl} />);
-//  }
-//}
-//
-//processUrl(window.location.href);
-
 import React from 'react'
 import {parse as parseUrl} from 'url';
 import {loadRemoteFile} from './_external-deps/http-get';
