60e544c3940673a8ca99e575fed5b7176ede5f83
Wolfram Kriesing <wk@uxebu.com>
2015-09-26 13:06:02 +0200

    All we can do now about solar system.

--- src/components/solar-system.js
+++ src/components/solar-system.js
@@ -5,14 +5,15 @@ export default class SolarSystemComponent {
 
   render() {
     return (
-    	<main role="main" class="pure-u-2-3">
-    		<div id="featured" class="solsys center">
+    	<main role="main" className="pure-u-2-3">
+    		<div id="featured" className="solsys center">
     			<h1>Sonnensystem</h1>
     			<p>unsere kosmische Heimat</p>
     		</div>
     		<script type="text/javascript" src="js/solsysTable.js"></script>
-    		<div id="dataTable" class="solsys">
+    		<div id="dataTable" className="solsys">
     			<div id="solsysTable">
+            missing data ... got no JSON file for this :(
           </div>
         </div>
         <Notes />
@@ -20,24 +21,6 @@ export default class SolarSystemComponent {
     );
   }
 }
-//<?php include("header.php")?>
-//	<main role="main" class="pure-u-2-3">
-//		<div id="featured" class="solsys center">
-//			<h1>Sonnensystem</h1>
-//			<p>unsere kosmische Heimat</p>
-//		</div>
-//		<script type="text/javascript" src="js/solsysTable.js"></script>
-//		<div id="dataTable" class="solsys">
-//			<div id="solsysTable">
-//
-//<?php
-////$link = mysqli_connect("localhost", "d01d6726", "tAdbJYXJhykvY7bR", "d01d6726");
-////mysqli_set_charset($link, "utf8");
-////// check connection
-////if (mysqli_connect_errno()) {
-////	printf("Keine Verbindung zur Datenbank: %s\n", mysqli_connect_error());
-////	exit();
-////}
 ////$query = "SELECT  FROM solsys ORDER by order";
 ////if ($result = mysqli_query($link, $query)) {
 ////// fetch object array
@@ -52,18 +35,3 @@ export default class SolarSystemComponent {
 ////						<div class=\"solsysInfo\">$info</div>
 ////					</div>
 ////				</div>";
-////	}
-////// free result set
-////	mysqli_free_result($result);
-////}
-////// close connection
-////mysqli_close($link);
-//?>
-//			</div>
-//		</div>
-//		<!--<script src="http://de.cosmowiki.de/js/solsysTableData.json"></script>-->
-//		<div id="notes">
-//<?php include("notes.php")?>
-//		</div>
-//	</main><!-- #main -->
-//<?php include("footer.php")?>
