6da7b8a69ab74909a6b675f7da3051437141aba6
Wolfram Kriesing <wk@uxebu.com>
2015-10-04 21:02:02 +0200

    Pass on one parameter to update a single json file. Fixes #12.

--- data/update-data.sh
+++ data/update-data.sh
@@ -2,30 +2,44 @@
 
 echo "downloading data"
 
-FILENAME=data/chronicle.json
-gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 0
-echo "`du -h $FILENAME` loaded"
+if [[ $# == 0 || $1 == chronicle ]]; then
+  FILENAME=data/chronicle.json
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 0
+  echo "`du -h $FILENAME` loaded";
+fi
 
-FILENAME=data/people.json
-gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 1
-echo "`du -h $FILENAME` loaded"
+if [[ $# == 0 || $1 == people ]]; then
+  FILENAME=data/people.json
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 1
+  echo "`du -h $FILENAME` loaded"
+fi
 
-FILENAME=data/places.json
-gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 2
-echo "`du -h $FILENAME` loaded"
+if [[ $# == 0 || $1 == places ]]; then
+  FILENAME=data/places.json
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 2
+  echo "`du -h $FILENAME` loaded"
+fi
 
-FILENAME=data/missions.json
-gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 3
-echo "`du -h $FILENAME` loaded"
+if [[ $# == 0 || $1 == missions ]]; then
+  FILENAME=data/missions.json
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 3
+  echo "`du -h $FILENAME` loaded"
+fi
 
-FILENAME=data/stars.json
-gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 4
-echo "`du -h $FILENAME` loaded"
+if [[ $# == 0 || $1 == stars ]]; then
+  FILENAME=data/stars.json
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 4
+  echo "`du -h $FILENAME` loaded"
+fi
 
-FILENAME=data/constellations.json
-gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 5
-echo "`du -h $FILENAME` loaded"
+if [[ $# == 0 || $1 == constellations ]]; then
+  FILENAME=data/constellations.json
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 5
+  echo "`du -h $FILENAME` loaded"
+fi
 
-FILENAME=data/spacestations.json
-gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 6
-echo "`du -h $FILENAME` loaded"
+if [[ $# == 0 || $1 == spacestations ]]; then
+  FILENAME=data/spacestations.json
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 6
+  echo "`du -h $FILENAME` loaded"
+fi
