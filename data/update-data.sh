74af221479c6eba3ac22624acb3f36109597fd03
Wolfram Kriesing <wk@uxebu.com>
2015-10-05 10:29:13 +0200

    Let's store the JSON data in a beautified way, so we can better see diffs as long as we still have to work with those data in the repo.

--- data/update-data.sh
+++ data/update-data.sh
@@ -4,42 +4,42 @@ echo "downloading data"
 
 if [[ $# == 0 || $1 == chronicle ]]; then
   FILENAME=data/chronicle.json
-  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 0
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 0
   echo "`du -h $FILENAME` loaded";
 fi
 
 if [[ $# == 0 || $1 == people ]]; then
   FILENAME=data/people.json
-  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 1
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 1
   echo "`du -h $FILENAME` loaded"
 fi
 
 if [[ $# == 0 || $1 == places ]]; then
   FILENAME=data/places.json
-  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 2
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 2
   echo "`du -h $FILENAME` loaded"
 fi
 
 if [[ $# == 0 || $1 == missions ]]; then
   FILENAME=data/missions.json
-  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 3
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 3
   echo "`du -h $FILENAME` loaded"
 fi
 
 if [[ $# == 0 || $1 == stars ]]; then
   FILENAME=data/stars.json
-  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 4
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 4
   echo "`du -h $FILENAME` loaded"
 fi
 
 if [[ $# == 0 || $1 == constellations ]]; then
   FILENAME=data/constellations.json
-  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 5
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 5
   echo "`du -h $FILENAME` loaded"
 fi
 
 if [[ $# == 0 || $1 == spacestations ]]; then
   FILENAME=data/spacestations.json
-  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM $FILENAME -w 6
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 6
   echo "`du -h $FILENAME` loaded"
 fi
