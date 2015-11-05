a38b554cbc12d10133d714c52ef9c8e095e9d7a4
Wolfram Kriesing <wolfram+github@kriesing.de>
2015-11-06 00:53:14 +0100

    Reorder tests a bit.

--- test/scripts/copy-and-minify-json-files-spec.js
+++ test/scripts/copy-and-minify-json-files-spec.js
@@ -25,29 +25,34 @@ function minifyJson(content) {
   return JSON.stringify(JSON.parse(content));
 }
 
-describe('converting `stars.json`', () => {
-
-  const destFile = path.join(__dirname, '../../dist/data/stars.json');
-  const sourceFile = path.join(__dirname, '../../data/stars.json');
-  let promise; 
- 
-  beforeEach(() => { 
-    if (fs.existsSync(destFile)) fs.unlinkSync(destFile);
-    promise = convertOneFile(sourceFile, destFile);
-  });
-
-  it('fulfills', () => {
-    return promiseThat(promise, is(fulfilled()));
-  });
-
-  it('writes the destination file', () => {
-    return promise.then(() => assert(fs.existsSync(destFile), 'Destination file missing.'));
-  });
-
-  it('is minified', () => {
-    return promise.then(() => {
-      const jsonFile = fs.readFileSync(destFile, 'utf8');
-      assertThat(jsonFile, is(JSON.stringify(JSON.parse(jsonFile))))
+describe('converting', () => {
+  
+  describe('existing file `stars.json`', () => {
+  
+    const destFile = path.join(__dirname, '../../dist/data/stars.json');
+    const sourceFile = path.join(__dirname, '../../data/stars.json');
+    let promise; 
+  
+    beforeEach(() => { 
+      if (fs.existsSync(destFile)) fs.unlinkSync(destFile);
+      promise = convertOneFile(sourceFile, destFile);
     });
+  
+    it('returns a promise that fulfills', () => {
+      return promiseThat(promise, is(fulfilled()));
+    });
+  
+    it('writes the destination file', () => {
+      return promise.then(() => assert(fs.existsSync(destFile), 'Destination file missing.'));
+    });
+  
+    it('is minified', () => {
+      return promise.then(() => {
+        const jsonFile = fs.readFileSync(destFile, 'utf8');
+        assertThat(jsonFile, is(JSON.stringify(JSON.parse(jsonFile))))
+      });
+    });
+    
   });
+  
 });
