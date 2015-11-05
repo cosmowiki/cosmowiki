8a919718ab2208e5b4f9b7ca458c3ec884d6690d
Wolfram Kriesing <wolfram+github@kriesing.de>
2015-11-05 22:30:14 +0100

    A spike to make json minifying work for one file.

--- test/scripts/copy-and-minify-json-files-spec.js
+++ test/scripts/copy-and-minify-json-files-spec.js
@@ -1,21 +1,61 @@
-describe('given, a set of files', function() {
+import assert from 'assert';
+import {
+  assertThat,
+  everyItem,
+  hasProperty,
+  endsWith,
+  hasSize,
+  promiseThat, is, fulfilled, fulfilledWith,
+  allOf
+} from 'hamjest';
 
-  let originalFiles;
-  beforeEach(function() {
-    const jsonFilePromise = Promise.resolve({fileName: '/file/name.json', content: '{}'});
-    const txtFilePromise = Promise.resolve({fileName: '/file/name.txt', content: ' '});
-    originalFiles = [
-      jsonFilePromise,
-      txtFilePromise
-    ];
+import fs from 'fs';
+import path from 'path';
+
+function convertOneFile(fileName, destFileName) {
+  const fileReadPromise = new Promise(function(resolve, reject) {
+    fs.readFile(fileName, 'utf8', (error, content) => {
+      if (!error) resolve(content);
+
+    });
   });
-  
-  it('only the JSON files are returned', function() {
-    
+  return fileReadPromise.then((fileContent) => {
+    return new Promise(resolve => {
+      fs.writeFile(destFileName, minifyJson(fileContent), 'utf8', resolve);
+    });
   });
-  
-  it('all JSON files get minified', function() {
-    
+}
+
+function minifyJson(content) {
+  return content;
+  return JSON.stringify(JSON.parse(content));
+}
+
+describe('converting `stars.json`', () => {
+
+  const destFile = path.join(__dirname, '../../dist/data/stars.json');
+  const sourceFile = path.join(__dirname, '../../data/stars.json');
+  let promise; 
+ 
+  beforeEach(() => { 
+    if (fs.existsSync(destFile)) fs.unlinkSync(destFile);
+    promise = convertOneFile(sourceFile, destFile);
   });
-  
+
+  it('fulfills', () => {
+    return promiseThat(promise, is(fulfilled()));
+  });
+
+  it('write the destination file', () => {
+    return promiseThat(promise, is(allOf(fulfilled(), is(fs.existsSync(destFile)))));
+  });
+
+  it('is minified', (done) => {
+    convertOneFile(sourceFile, destFile).then(() => {
+      const jsonFile = fs.readFileSync(destFile);
+      assert.equal(jsonFile, JSON.stringify(JSON.parse(jsonFile)));
+      done();
+    }).catch(done => done(new Error('convert threw')));
+  });
+
 });
