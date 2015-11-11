78b4dc4082562c499aaffbea1b94acf235efc9bc
Wolfram Kriesing <wolfram+github@kriesing.de>
2015-11-11 11:03:44 +0100

    Remove unused file.s

--- src/_external-deps/command-line-arguments.js
+++
@@ -1,3 +0,0 @@
-export function firstCommandLineArgument() {
-  return process.argv[2];
-}
--- src/_external-deps/read-file-sync.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-
-export function readFileSync(fileName) {
-  return fs.readFileSync(fileName, 'utf8');
-}
