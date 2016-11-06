a0cbc6782661e0707a622843f5667d3256ed9c47
cosmowiki <dev@cosmowiki.org>
2016-11-06 16:47:42 +0100

    branding pure width:100%

--- src/components/logo.js
+++ src/components/logo.js
@@ -3,7 +3,7 @@ export default class Logo {
   
   render() {
     return (
-      <div id="branding">
+      <div id="branding" className="pure-u-1">
         <a href="/" title="CosmoWiki.de" rel="home">
           <img src="/img/cosmowiki_30px.jpg" name="CosmoWiki" alt="CosmoWiki"/>
         </a>
