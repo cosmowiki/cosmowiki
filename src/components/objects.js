f808e5f1d70686abffc6aeab6732cee46b621c08
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-11-20 00:09:49 +0100

    Convert all to latest react.

--- src/components/objects.js
+++ src/components/objects.js
@@ -1,25 +1,20 @@
 import React from 'react';
 
-export default class ObjectsComponent {
-  
-  render() {
-    
-    let appUrl = this.props.appUrl;
-    
-    return (
-      <main role="main" className="pure-u-1">
-        <div id="featured" className="objects center">
-          <h1>Objekte</h1>
-          <h3>Himmelsk&ouml;rper, Raumfahrzeuge, Instrumente</h3>
-        </div>
-        <div id="subItemsContainer">
-          <a className="pure-u-1 subItem objects solsys center" href={appUrl.solarSystemSite()}>Sonnensystem</a>
-          <a className="pure-u-1 subItem objects constellations center" href={appUrl.constellationsSite()}>Sternbilder</a>
-          <a className="pure-u-1 subItem objects stars center" href={appUrl.starsSite()}>Sterne</a>
-          <a className="pure-u-1 subItem objects stations center" href={appUrl.spaceStationsSite()}>Raumstationen</a>
-        </div>
-      </main>
-    );
-  }
-  
-}
+const ObjectsComponent = ({appUrl}) => {
+  return (
+    <main role="main" className="pure-u-1">
+      <div id="featured" className="objects center">
+        <h1>Objekte</h1>
+        <h3>Himmelsk&ouml;rper, Raumfahrzeuge, Instrumente</h3>
+      </div>
+      <div id="subItemsContainer">
+        <a className="pure-u-1 subItem objects solsys center" href={appUrl.solarSystemSite()}>Sonnensystem</a>
+        <a className="pure-u-1 subItem objects constellations center" href={appUrl.constellationsSite()}>Sternbilder</a>
+        <a className="pure-u-1 subItem objects stars center" href={appUrl.starsSite()}>Sterne</a>
+        <a className="pure-u-1 subItem objects stations center" href={appUrl.spaceStationsSite()}>Raumstationen</a>
+      </div>
+    </main>
+  );
+};
+
+export default ObjectsComponent;
