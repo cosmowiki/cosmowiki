6a1b58caf0ea5b8ab7f2e257acfd1f1414681800
cosmowiki <dev@cosmowiki.org>
2016-11-29 02:49:47 +0100

    notes shortened

--- src/components/notes.js
+++ src/components/notes.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 const defaultNotes = (
   <p id="notComplete" className="center">
-    Diese Liste ist nicht vollst&auml;ndig und wird st&auml;ndig erweitert.<br/>
-    Sie kennen noch mehr Daten zu dieser Seite? Bereichern Sie dieses Lexikon und melden Sie uns diese bitte <a
+    Sie kennen noch mehr Daten zu dieser Seite?<br/>Bereichern Sie cosmowiki.org und melden Sie uns diese bitte <a
     href="http://de.cosmowiki.de/?page_id=22" target="_self" title="Kontakt">hier</a>.
   </p>
 );
