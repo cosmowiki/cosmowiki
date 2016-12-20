2ddc4b2db1e98c060351d617c4d48f65dd7e3c26
cosmowiki <dev@cosmowiki.org>
2016-12-21 00:25:24 +0100

    added a home-footer

--- src/components/content-only.js
+++ src/components/content-only.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Logo from './logo'
+import Homefooter from './homefooter';
 
 const ContentOnlyComponent = ({children, appUrl}) => {
   return (
     <div id="wrapper" className="pure-g home">
       <Logo/>
       {children}
+      <Homefooter appUrl={appUrl} />
     </div>
   )
 };
---
+++ src/components/homefooter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homefooter = ({appUrl}) => {
+  return (
+    <div id="homeFooter" className="pure-u-1 home center">
+      v. 1.0
+    </div>
+  );
+};
+
+export default Homefooter;
