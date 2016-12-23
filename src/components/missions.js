930f747fba7bb005c397098d741a8d8df80a0f80
cosmowiki <dev@cosmowiki.org>
2016-12-23 18:21:32 +0100

    responsive!
    todo: bold the „Ende:“ and „Ziel:“

--- src/components/missions.js
+++ src/components/missions.js
@@ -9,7 +9,8 @@ const MissionsComponent = ({missions}) => {
         <h3>auf zu neuen Ufern</h3>
       </div>
       <div id="todo">
-        pls add functions for the sorter and filters
+        pls add functions for the sorter and filters<br />
+        pls set the "Ende:" and "Land:" bold. - f*** syntax
       </div>
       <div id="functionArea" className="missions">
         <div id="sort" className="missions">
@@ -70,20 +71,16 @@ export default MissionsComponent;
 const MissionComponent = ({mission}) => {
   return (
     <div className="missionsRow pure-u-1">
-      <div className="missionName pure-u-1 pure-u-sm-1-2 pure-u-md-1-3 pure-u-lg-9-24 center">
+      <div className="missionName pure-u-1 pure-u-sm-9-24">
         <a href={mission.link}>{mission.name}</a>
       </div>
-      <div className="missionLaunch pure-u-1-2 pure-u-sm-1-4 pure-u-md-1-3 pure-u-lg-3-24 center">
-        <p>Start: {mission.launchDate}</p>
+      <div className="missionDates pure-u-1-2 pure-u-sm-6-24">
+        <p className="missionLaunch pure-u-lg-1-2"><b>Start:</b> {mission.launchDate}</p>
+        <p className="missionEnd">{mission.endDate ? `Ende: ${mission.endDate}` : `Status: ${mission.status}`}</p>
       </div>
-      <div className="missionEnd pure-u-1-2 pure-u-sm-1-4 pure-u-md-1-3 pure-u-lg-3-24 center">
-        <p>{mission.endDate ? `Ende: ${mission.endDate}` : `Status: ${mission.status}`}</p>
-      </div>
-      <div className="missionOperator pure-u-1-2 pure-u-sm-1-4 pure-u-md-1-3 pure-u-lg-4-24 center">
-        {mission.operator ? <p>Betreiber: {mission.operator} - {mission.country}</p> : <p>Land: {mission.country}</p>}
-      </div>
-      <div className="missionDestination pure-u-1-2 pure-u-sm-1-4 pure-u-md-1-3 pure-u-lg-5-24 center">
-        <p>Ziel: {mission.destination}</p>
+      <div className="missionInfo pure-u-1-2 pure-u-sm-9-24">
+        <p className="missionOperator">{mission.operator ? `Betreiber: ${mission.operator} - ${mission.country}` : `Land: ${mission.country}`}</p>
+        <p className="missionDestination"><b>Ziel:</b> {mission.destination}</p>
       </div>
     </div>
   );
