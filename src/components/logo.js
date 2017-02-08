9dc7d6e0dfcee2d8467c6b1112e99bec5e5baf9f
cosmowiki <dev@cosmowiki.org>
2017-02-08 21:22:12 +0100

    prepared for search and responsive menu

--- src/components/logo.js
+++ src/components/logo.js
@@ -3,9 +3,17 @@ import React from 'react';
 const Logo = () => {
   return (
     <div id="branding" className="pure-u-1">
-      <a href="/" title="CosmoWiki.de" rel="home">
-        <img src="/img/cosmowiki_30px.jpg" id="CosmoWikiLogo" alt="CosmoWiki"/>
-      </a>
+      <div id="logo" className="pure-u-1-3">
+        <a href="/" title="CosmoWiki.de" rel="home">
+          <img src="/img/cosmowiki_30px.jpg" id="CosmoWikiLogo" alt="CosmoWiki"/>
+        </a>
+      </div>
+      <div id="search" className="pure-u-1-3 center">
+        Suche
+      </div>
+      <div id="menu" className="pure-u-1-3 right">
+        Men&uuml;
+      </div>
     </div>
   );
 };
