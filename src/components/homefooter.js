7e65d3055e8c6a47afa53aed6d2e67a3d6f70787
cosmowiki <dev@cosmowiki.org>
2017-02-13 10:46:08 +0100

    homefooter <p>

--- src/components/homefooter.js
+++ src/components/homefooter.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Homefooter = ({appUrl}) => {
   return (
     <div id="homeFooter" className="pure-u-1 center">
-      v. 1.0
+      <p>v. 1.0</p>
     </div>
   );
 };
