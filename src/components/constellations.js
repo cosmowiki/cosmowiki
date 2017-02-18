e85d3f020b68101e48597ac13841e307a03501e8
cosmowiki <dev@cosmowiki.org>
2017-02-18 04:33:43 +0100

    ConstellationNotes

--- src/components/constellations.js
+++ src/components/constellations.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Notes from './notes';
+import {ConstellationNotes} from './notes';
 
 const ConstellationsComponent = ({constellations}) => {
   return (
@@ -37,7 +37,7 @@ const ConstellationsComponent = ({constellations}) => {
           {constellations.map((constellation, idx) => <ConstellationComponent constellation={constellation} key={idx}/>)}
         </div>
       </div>
-      <Notes />
+      <ConstellationNotes />
     </main>
   );
 };
