e07eca207c97c2e173d3119b81064afd00b64876
Wolfram Kriesing <wolfram+github@kriesing.de>
2017-02-21 12:55:10 +0100

    Render all telescopes.

--- src/components/space-telescopes.js
+++ src/components/space-telescopes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const SpaceTelescopesComponent = () => {
+const SpaceTelescopesComponent = ({ telescopes }) => {
 
   return (
     <main role="main" className="pure-u-2-3">
@@ -9,13 +9,18 @@ const SpaceTelescopesComponent = () => {
         <p>Augen und Ohren im All</p>
       </div>
       <div id="dataArea" className="spacetele">
-        <div style={ { textAlign: 'center', position: 'relative', top: '10px'} }>
-          <img src="http://cosmowiki.de/img/loading.gif" alt="" title="Bitte warten, Daten werden geladen."/><br />
-          Bitte warten, Daten werden geladen.
-        </div>
+        {telescopes.map((telescope, idx) => <TelescopeComponent telescope={ telescope } key={idx}/>)}
       </div>
     </main>
   );
 };
 
 export default SpaceTelescopesComponent;
+
+const TelescopeComponent = ({ telescope }) => {
+  return (
+    <div>
+      {telescope.name}
+    </div>
+  );
+};
