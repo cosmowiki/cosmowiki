6c65c442da262777c2b730f82b7d0d74b8357888
cosmowiki <dev@cosmowiki.org>
2017-02-22 09:28:54 +0100

    spacetelescopes page close to finished

--- src/components/space-telescopes.js
+++ src/components/space-telescopes.js
@@ -1,16 +1,45 @@
 import React from 'react';
+import Notes from './notes';
 
 const SpaceTelescopesComponent = ({ telescopes }) => {
 
   return (
-    <main role="main" className="pure-u-2-3">
-      <div id="featured" className="spacetele">
+    <main role="main" className="pure-u-1">
+      <div id="featured" className="spacetelecopes pure-u-1 center">
         <h1>Weltraumteleskope</h1>
-        <p>Augen und Ohren im All</p>
+        <h3>Augen und Ohren im All</h3>
       </div>
-      <div id="dataArea" className="spacetele">
-        {telescopes.map((telescope, idx) => <TelescopeComponent telescope={ telescope } key={idx}/>)}
+      <div id="todo" className="pure-u-1">
+        <p>@wolfram pls edit the month in endDate to appear as word when there is no endday</p>
       </div>
+      <div id="controlArea" className="spacetelecopes pure-u-1">
+        <div id="controllers" className="pure-u-1">
+          <div id="sort" className="spacetelecopes pure-u-1-2 left">
+            <a href="#" className="toggle-sort" name="toggle-sort">Sortieren</a>
+            <div id="sortArea">
+              <form id="sortSpacetelecopes" className="sort-form">
+                <label>Sortieren nach:</label>
+                <select name="sortSpacetelecopes" defaultValue="sortSpacetelecopesLaunchUp">
+                  <option value="sortSpacetelecopesLaunchUp">Startdatum &uarr;</option>
+                  <option value="sortSpacetelecopesLaunchDown">Startdatum &darr;</option>
+                  <option value="sortSpacetelecopesNameUp">Name &uarr;</option>
+                  <option value="sortSpacetelecopesNameDown">Name &darr;</option>
+                  <option value="sortSpacetelecopesEndUp">Missionsende &uarr;</option>
+                  <option value="sortSpacetelecopesEndDown">Missionsende &darr;</option>
+                  <option value="sortSpacetelecopesDurationUp">Dauer &uarr;</option>
+                  <option value="sortSpacetelecopesDurationDown">Dauer &darr;</option>
+                </select>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="dataArea" className="spacetelescopes pure-u-1">
+        <div id="spacetelescopesTable">
+          {telescopes.map((telescope, idx) => <TelescopeComponent telescope={ telescope } key={idx}/>)}
+        </div>
+      </div>
+      <Notes/>
     </main>
   );
 };
@@ -19,8 +48,16 @@ export default SpaceTelescopesComponent;
 
 const TelescopeComponent = ({ telescope }) => {
   return (
-    <div>
-      {telescope.name}
+    <div className="spacetelescope-row data-row pure-u-1">
+      <div className="spacetelescopeImg pure-u-1 pure-u-sm-1-5 center">
+        {telescope.imageUrl ? <a href={telescope.imageUrl}><img src={telescope.imageSmallUrl} alt={telescope.name} /></a> : ''}
+        {telescope.imageUrl ? <small>Bild: {telescope.imageSrc}</small> : ''}
+        {telescope.imageLicenseUrl ? <small><a href={telescope.imageLicenseUrl}>{telescope.imageLicence}</a></small> : <small>{telescope.imageLicence}</small>}
+      </div>
+      <div className="spacetelescopeInfo pure-u-1 pure-u-sm-3-5 center">
+        <a href={telescope.wikipediaUrl}>{telescope.name}</a><br />
+        {telescope.endDate ? `von ${telescope.launchDate} bis ${telescope.endDate}` : `Start: ${telescope.launchDate}`}
+      </div>
     </div>
   );
 };
--- src/sites/space-telescopes.js
+++ src/sites/space-telescopes.js
@@ -16,8 +16,86 @@ export default class SpaceTelescopes {
 class Telescope {
 
   static fromRawData(raw) {
-    const station = new Telescope();
-    station.name = raw.itemname;
-    return station;
+    const telescope = new Telescope();
+    telescope.name = raw.itemname;
+    telescope.alternativename = raw.itemname2 ? raw.itemname2 : '';
+    telescope.wikipediaUrl = raw.itemurl ? raw.itemurl : '';
+    telescope.id = raw.itemid;
+    telescope.imageSmallUrl = raw.itemimgsmallurl;
+    telescope.imageUrl = raw.itemimgurl;
+    telescope.imageSrc = raw.itemimgsrc;
+    telescope.imageLicence = raw.itemimglicence;
+    telescope.imageLicenseUrl = raw.itemimglicenceurl;
+
+    const launchyear = raw.itemdateyear ? raw.itemdateyear : '';
+    const launchmonth = raw.itemdatemonth ? `${raw.itemdatemonth}.` : '';
+    const launchday = raw.itemdateday ? `${raw.itemdateday}.` : '';
+    telescope.launchDate = `${launchday}${launchmonth}${launchyear}`;
+    const endyear = raw.itemdate2year ? raw.itemdate2year : '';
+    const endmonth = raw.itemdate2month ? `${raw.itemdate2month}.` : '';
+    const endday = raw.itemdate2day ? `${raw.itemdate2day}.` : '';
+    telescope.endDate = `${endday}${endmonth}${endyear}`;
+    telescope.duration = raw.itemduration ? raw.itemduration : '';
+    telescope.rocket = Rocket.fromRawData(raw);
+    telescope.pad = Pad.fromRawData(raw);
+    telescope.status = raw.itemstatus;
+    telescope.country = raw.itemcountry;
+    telescope.operator = raw.itemoperator;
+    telescope.purpose = raw.itempurpose;
+
+    return telescope;
   }
+
 }
+
+class Rocket {
+
+  static fromRawData(raw) {
+    const rocket = new Rocket();
+
+    rocket.name = raw.itemparent;
+    rocket.wikipediaUrl = raw.itemparenturl;
+
+    return rocket;
+  }
+
+}
+
+class Pad {
+
+  static fromRawData(raw) {
+    const pad = new Pad();
+
+    pad.name = raw.itemlocation;
+    pad.wikipediaUrl = raw.itemlocationurl;
+
+    return pad;
+  }
+
+}
+
+/*
+{
+    "itemname": "Uhuru",
+    "itemname2": "SAS-1, Explorer 42 ",
+    "itemurl": "https://de.wikipedia.org/wiki/Uhuru_%28Satellit%29",
+    "itemid": "1970-107A",
+    "itemimgsmallurl": "/img/spacetelescopes/Uhuru_sm.gif",
+    "itemimgurl": "/img/spacetelescopes/Uhuru_lg.gif",
+    "itemimgsrc": "NASA",
+    "itemimglicence": "gemeinfrei",
+    "itemdateyear": "1970",
+    "itemdatemonth": 12,
+    "itemdateday": 12,
+    "itemdate2year": 1973,
+    "itemdate2month": 3,
+    "itemdate2year": 1979,
+    "itemdate2month": 4,
+    "itemdate2day": 5,
+    "itemduration": "2a 78d",
+    "itemstatus": "beendet",
+    "itemcountry": "USA",
+    "itemoperator": "NASA",
+    "itempurpose": "Röntgen (2 keV – 20 keV)"
+},
+*/
