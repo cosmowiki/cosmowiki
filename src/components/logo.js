e73735b574d6d93d1f23d94fa70e8d411dc97fa9
cosmowiki <dev@cosmowiki.org>
2017-02-27 11:56:12 +0100

    style of logo and search form

--- src/components/logo.js
+++ src/components/logo.js
@@ -3,18 +3,18 @@ import React from 'react';
 const Logo = () => {
   return (
     <div id="branding" className="pure-u-1">
-      <div id="logo" className="pure-u-1-6 pure-u-sm-1-4 pure-u-md-1-3">
+      <div id="logo" className="pure-u-1-8 pure-u-sm-1-4 pure-u-md-1-3">
         <a href="/" title="CosmoWiki.de" rel="home">
           <img src="/img/cosmowiki_30px.jpg" id="CosmoWikiLogo" alt="CosmoWiki"/>
           <img src="/img/cw_icon_30px.jpg" id="CosmoWikiIcon" alt="CosmoWiki"/>
         </a>
       </div>
-      <div id="search" className="pure-u-2-3 pure-u-sm-1-2 pure-u-md-1-3 center">
+      <div id="searchArea" className="pure-u-3-4 pure-u-sm-1-2 pure-u-md-1-3 center">
         <form id="searchForm" role="search" method="get" action="">
-          <input id="searchInput" className="pure-u-1" type="text" name="s" value="" placeholder="CosmoWiki durchsuchen" required />
+          <input id="search" className="pure-u-1" name="search" type="text" placeholder="CosmoWiki durchsuchen" required />
         </form>
       </div>
-      <div id="menuToggle" className="pure-u-1-6 pure-u-sm-1-4 pure-u-md-1-3 right">
+      <div id="menuToggle" className="pure-u-1-8 pure-u-sm-1-4 pure-u-md-1-3 right">
         <a href="#"></a>
       </div>
     </div>
