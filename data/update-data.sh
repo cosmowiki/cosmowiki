f6402bec0eb67f2192d9c31612b77c325fa5c24a
cosmowiki <dev@cosmowiki.org>
2017-04-03 17:38:36 +0200

    new JSON for EVAs (spacewalks)

--- data/update-data.sh
+++ data/update-data.sh
@@ -61,3 +61,9 @@ if [[ $# == 0 || $1 == solarsystem ]]; then
   gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 9
   echo "`du -h $FILENAME` loaded"
 fi
+
+if [[ $# == 0 || $1 == eva ]]; then
+  FILENAME=data/eva.json
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 10
+  echo "`du -h $FILENAME` loaded"
+fi
