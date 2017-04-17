db1dbdd01ccfdd7dcaa60af3c7bf33fa87d0f5a2
cosmowiki <dev@cosmowiki.org>
2017-04-17 06:37:55 +0200

    spacewalks summary finished

--- src/components/spacewalks.js
+++ src/components/spacewalks.js
@@ -8,58 +8,62 @@ const SpacewalksComponent = ({spacewalks}) => {
         <h1>Weltraumausstiege</h1>
         <h3>- völlig losgelöst -</h3>
       </div>
+      <div id="todo" className="pure-u-1">
+        <p>@Wolfram pls make the #summaryToggleSwitch work.</p>
+      </div>
       <div id="summary" className="spacewalks pure-u-1 left">
-        <div id="summaryContainer" className="spacewalks">
-          <div className="summary-img">
-            <a href="/img/spacewalks/eva2_lg.jpg" title="Buzz Aldrin auf dem Mond">
-              <img src="/img/spacewalks/eva2_sm.jpg" />
-            </a>
-            <div className="summary-img-src small">NASA</div>
-          </div>
-          <p>
-            <b><i>Der Weltraumausstieg</i></b>, auch <i>EVA</i> (extra-vehicular activity = Außenbordaktivität)
+        <a id="summaryToggleSwitch" className="expanded" href="#" title="Artikel anzeigen / schließen"></a>
+        <div id="summaryContainer" className="spacewalks visible">
+          <p className="summary-text">
+            <i>Der Weltraumausstieg</i>, auch <i>EVA</i> (extra-vehicular activity = Außenbordaktivität)
             oder <i>spacewalk</i> genannt, ist ein Begriff für den Aufenthalt von
             Astronauten außerhalb eines Raumfahrzeuges im Vakuum des Weltalls oder
             auf einem anderen Himmelskörper. (z. B. Lunar Extra Vehicular Activity = LEVA)
             Er wird meist für Arbeiten an einem Raumfahrzeug, zu wissenschaftlichen
             Zwecken oder zur Erprobung neuer Techniken durchgeführt.
           </p>
-          <p>
+          <div className="summary-img float-right">
+            <a href="/img/spacewalks/eva2_lg.jpg" title="Buzz Aldrin auf dem Mond">
+              <img src="/img/spacewalks/eva2_sm.jpg" alt="Großansicht" />
+            </a>
+            <p className="summary-img-src small">NASA</p>
+          </div>
+          <p className="summary-text">
             Die Raumfahrer sind hierbei nur durch einen speziellen Raumanzug vor
             dem Vakuum und der Strahlung des Weltraums sowie vor Mikrometeoriten geschützt.
             Die Strahlenbelastung ist dabei etwa doppelt so hoch wie im Raumfahrzeug.
           </p>
-          <div className="summary-img">
-            <a href="/img/spacewalks/eva4_lg.jpg" title="Juri Onufrijenko an der Raumstation Mir">
-              <img src="/img/spacewalks/eva4_sm.jpg" />
-            </a>
-            <div className="summary-img-src small">NASA</div>
-          </div>
-          <p>
+          <p className="summary-text">
             Bei der so genannten <i>Stand-Up-EVA</i> (SEVA) steht ein Raumfahrer in der
             geöffneten Luke seines Raumfahrzeugs ohne dies zu verlassen. Er ist hierbei
             vollständig abhängig vom den Systemen des Raumanzugs und assistiert
             einem weiteren Raumfahrer bei dessen EVA oder filmt ihn dabei.
           </p>
-          <p>
+          <div className="summary-img float-left">
+            <a href="/img/spacewalks/eva4_lg.jpg" title="Juri Onufrijenko an der Raumstation Mir">
+              <img src="/img/spacewalks/eva4_sm.jpg" alt="Großansicht" />
+            </a>
+            <p className="summary-img-src small">NASA</p>
+          </div>
+          <p className="summary-text">
             Wirklich freie Aufenthalte im All ohne Sicherungsleine wurden mit der <i>Manned Maneuvering Unit (MMU)</i>,
             einer Art Tornister mit kleinen Schubdüsen möglich. Sie werden nur in
             Ausnahmefällen durchgeführt. Der aktuelle Nachfolger der MMU ist
             das <i>SAFER</i>-System (Simplified Aid for EVA Rescue).
           </p>
-          <div className="summary-img">
-            <a href="/img/spacewalks/eva3_lg.jpg" title="Missionslogo von Woschod 2">
-              <img src="/img/spacewalks/eva3_sm.jpg" />
-            </a>
-            <div className="summary-img-src small">Quelle unbekannt</div>
-          </div>
-          <p>
+          <p className="summary-text">
             Der sowjetische Kosmonaut <i>Alexei Leonow</i> absolvierte am 18. März 1965
             den ersten Weltraumausstieg. Etwa zwölf Minuten lang schwebte er außerhalb
             von <i>Woschod 2</i> auf seiner eigenen Umlaufbahn um die Erde. Eine
             Kamera an der Außenseite des Raumschiffs übertrug das historische Ereignis.
           </p>
-          <p>
+          <div className="summary-img float-right">
+            <a href="/img/spacewalks/eva3_lg.jpg" title="Missionslogo von Woschod 2">
+              <img src="/img/spacewalks/eva3_sm.jpg" alt="Großansicht" />
+            </a>
+            <p className="summary-img-src small">Quelle unbekannt</p>
+          </div>
+          <p className="summary-text">
             Bei der Rückkehr in das Raumschiff ergaben sich große Schwierigkeiten,
             da sich der Berkut-Raumanzug durch den fehlenden Gegendruck im All aufgebläht
             hatte und zu steif geworden war. Erst als Leonow Druck aus seinem Anzug abgelassen
@@ -67,7 +71,7 @@ const SpacewalksComponent = ({spacewalks}) => {
             stieg seine Körpertemperatur um 3,2 °C an. In dem Buch "Zwei Mann im Mond" verarbeitete
             er seinen Weltraum"spaziergang" und die gesamte Mission.
           </p>
-          <p>
+          <p className="summary-text">
             Die erste Frau, die einen Außenbordeinsatz durchführte, war <i>Swetlana Sawizkaja</i> am
             25. Juli 1984 an Bord der sowjetischen Raumstation <i>Saljut 7</i>.
             Gemeinsam mit <i>Wladimir Dschanibekow</i> testete sie neue Werkzeuge
@@ -75,10 +79,16 @@ const SpacewalksComponent = ({spacewalks}) => {
             unter Weltraumbedingungen.
           </p>
           <h4>Wussten Sie schon?</h4>
-          <ul>
-            <li>Bis jetzt wurden [totalNumberOfItems] Weltraumausstiege von [totalNumberOfAstronauts] Astronauten durchgeführt.</li>
-            <li>[astronautMaxNumberOfItems] absolvierte mit [maxNumberOfItems] EVA die meisten Außenbordeinsätze.</li>
-            <li>Am [longestItemStartDate] führten [astronautsLongestItem] den mit [longestItemTime] bislang längsten Weltraumausstieg durch.</li>
+          <ul className="summary-list">
+            <li>
+              Bis jetzt wurden [totalNumberOfItems] Weltraumausstiege von [totalNumberOfAstronauts] Astronauten durchgeführt.
+            </li>
+            <li>
+              [astronautMaxNumberOfItems] absolvierte mit [maxNumberOfItems] EVA die meisten Außenbordeinsätze.
+            </li>
+            <li>
+              Am [longestItemStartDate] führten [astronautsLongestItem] den mit [longestItemTime] bislang längsten Weltraumausstieg durch.
+            </li>
           </ul>
         </div>
       </div>
@@ -90,8 +100,8 @@ const SpacewalksComponent = ({spacewalks}) => {
               <form id="sortSpacewalks" className="sort-form">
                 <label>Sortieren nach:</label>
                 <select name="sortSpacewalks" defaultValue="sortSpacewalksStartUp">
-                  <option value="sortSpacewalksStartUp">Startdatum &uarr;</option>
-                  <option value="sortSpacewalksStartDown">Startdatum &darr;</option>
+                  <option value="sortSpacewalksStartUp">Beginn &uarr;</option>
+                  <option value="sortSpacewalksStartDown">Beginn &darr;</option>
                   <option value="sortSpacewalksDurationUp">Dauer &uarr;</option>
                   <option value="sortSpacewalksDurationDown">Dauer &darr;</option>
                   <option value="sortSpacewalksAstronautsUp">Astronauten &uarr;</option>
