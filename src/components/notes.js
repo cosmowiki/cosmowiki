3a03f6a6739e6280c586a4b4f671b6a6ba23f25f
cosmowiki <dev@cosmowiki.org>
2017-09-09 01:17:12 +0200

    better responsive look for the notes

--- src/components/notes.js
+++ src/components/notes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const defaultNotes = (
-  <div id="defaultNotes">
+  <div id="notesDefault">
     <div id="notComplete" className="pure-u-1 center">
       <p>
         Sie kennen noch mehr Daten zu dieser Seite?<br/>
@@ -12,67 +12,70 @@ const defaultNotes = (
 );
 
 const constellationNotes = (
-  <div id="constellationNotes">
-    <p>
-      <b>Lage des Sternbildes innerhalb der <a href="https://de.wikipedia.org/wiki/Himmelskugel" target="_blank" title="Himmeskugel">Himmelskugel</a>:</b>
-    </p>
-    <p>
-      <i>N</i> - die Sternbilder liegen auf der Nordhalbkugel des Himmels,
-    </p>
-    <p>
-      <i>S</i> - die Sternbilder liegen auf der Südhalbkugel des Himmels,
-    </p>
-    <p>
-      <i>N S</i> und <i>S N</i> - die Sternbilder liegen etwa mittig auf dem Himmels&auml;quator,
-    </p>
-    <p>
-      <i>Ns</i> und <i>Sn</i> - nur ein kleiner Teil des Sternbildareals liegt auf der mit dem Kleinbuchstaben bezeichneten Halbkugel.
-    </p>
-    <p>
-      <b>Einheiten:</b>
-    </p>
-    <p>
-      <i>deg<sup>2</sup></i> - Fl&auml;che (Raumwinkel) des Sternbildes in <a href="https://de.wikipedia.org/wiki/Quadratgrad" target="_blank" title="Quadratgrad">Quadratgrad</a>,
-    </p>
-    <p>
-      <i>>3<sup>m</sup></i> - Zahl der Sterne im Sternbild mit <a href="https://de.wikipedia.org/wiki/Scheinbare_Helligkeit" target="_blank" title="scheinbare Helligkeit">scheinbarer Helligkeit</a> über 3 mag,
-    </p>
-    <p>
-      <i>>4<sup>m</sup></i> - Zahl der Sterne im Sternbild mit <a href="https://de.wikipedia.org/wiki/Scheinbare_Helligkeit" target="_blank" title="scheinbare Helligkeit">scheinbarer Helligkeit</a> über 4 mag,
-    </p>
-    <p>
-      <i>Mag<sub>max</sub></i> - <a href="https://de.wikipedia.org/wiki/Scheinbare_Helligkeit" target="_blank" title="scheinbare Helligkeit">scheinbare Helligkeit</a> des hellsten Sterns im Sternbild in mag.
-    </p>
+  <div id="notesConstellations">
+    <div id="notesUnits" className="pure-u-1">
+      <h4>Einheiten:</h4>
+      <p>
+        <i>deg<sup>2</sup></i> = <a href="https://de.wikipedia.org/wiki/Raumwinkel" target="_blank" title="Raumwinkel">Raumwinkel</a> (Fl&auml;che) in Quadratgrad
+      </p>
+      <p>
+        <i>mag</i> = <a href="https://de.wikipedia.org/wiki/Scheinbare_Helligkeit" target="_blank" title="scheinbare Helligkeit">scheinbare Helligkeit</a> in Magnitudo
+      </p>
+    </div>
   </div>
 );
 
 const starNotes = (
-  <div id="starNotes">
-    <p className="center">
-      Die Daten der einzelnen Sterne stammen aus den englischen Wikipedia-Artikeln, da diese meist umfangreicher und mit genaueren Quellenangaben belegt sind.
-    </p>
-    <p>
-      <b>Griechische Buchstaben:</b>
-    </p>
-    <p>
-      α = Alpha, β = Beta, γ = Gamma, δ = Delta, ε = Epsilon, ζ = Zeta, η = Eta, θ = Theta, ι = Iota, κ = Kappa, λ = Lambda,
-      μ = My, ν = Ny, ξ = Xi, ο = Omikron, π = Pi, ρ = Rho, σ = Sigma, τ = Tau, υ = Ypsilon, φ = Phi, χ = Chi, ψ = Psi, ω = Omega
-    </p>
-    <p>
-      <b>Einheiten:</b>
-    </p>
-    <p>
-      <i>Lj</i> = <a href="https://de.wikipedia.org/wiki/Lichtjahr" target="_blank" title="Lichtjahr">Entfernung Erde-Stern in Lichtjahren</a>,
-    </p>
-    <p>
-      <i>mag</i> = <a href="https://de.wikipedia.org/wiki/Scheinbare_Helligkeit" target="_blank" title="scheinbare Helligkeit">scheinbare Helligkeit</a> in mag,
-    </p>
-    <p>
-      <i>M<sub>&#9737;</sub></i> = <a href="https://de.wikipedia.org/wiki/Sonnenmasse" target="_blank" title="Sonnenmassen">Sonnenmassen</a>,
-    </p>
-    <p>
-      <i>R<sub>&#9737;</sub></i> = <a href="https://de.wikipedia.org/wiki/Sonnenradius" target="_blank" title="Sonnenradien">Sonnenradien</a>.
-    </p>
+  <div id="notesStars">
+    <div id="notesGlossary" className="pure-u-1 pure-u-lg-1-2">
+      <h4>Hinweise:</h4>
+      <p>
+        Die Daten der einzelnen Sterne stammen aus den englischen Wikipedia-Artikeln,
+        da diese meist umfangreicher und mit genaueren Quellenangaben belegt sind.
+      </p>
+      <h4>Griechische Buchstaben:</h4>
+      <div className="notesTable">
+        <div className="pure-u-1-3 pure-u-sm-1-4 pure-u-md-1-5 pure-u-lg-1-6">α = Alpha</div>
+        <div className="pure-u-1-3 pure-u-sm-1-4 pure-u-md-1-5 pure-u-lg-1-6">β = Beta</div>
+        <div className="pure-u-1-3 pure-u-sm-1-4 pure-u-md-1-5 pure-u-lg-1-6">γ = Gamma</div>
+        <div className="pure-u-1-3 pure-u-sm-1-4 pure-u-md-1-5 pure-u-lg-1-6">δ = Delta</div>
+        <div className="pure-u-1-3 pure-u-sm-1-4 pure-u-md-1-5 pure-u-lg-1-6">ε = Epsilon</div>
+        <div className="pure-u-1-3 pure-u-sm-1-4 pure-u-md-1-5 pure-u-lg-1-6">ζ = Zeta</div>
+        <div className="pure-u-1-3 pure-u-sm-1-4 pure-u-md-1-5 pure-u-lg-1-6">η = Eta</div>
+        <div className="pure-u-1-3 pure-u-sm-1-4 pure-u-md-1-5 pure-u-lg-1-6">θ = Theta</div>
+        <div className="pure-u-1-3 pure-u-sm-1-4 pure-u-md-1-5 pure-u-lg-1-6">ι = Iota</div>
+        <div className="pure-u-1-3 pure-u-sm-1-4 pure-u-md-1-5 pure-u-lg-1-6">κ = Kappa</div>
+        <div className="pure-u-1-3 pure-u-sm-1-4 pure-u-md-1-5 pure-u-lg-1-6">λ = Lambda</div>
+        <div className="pure-u-1-3 pure-u-sm-1-4 pure-u-md-1-5 pure-u-lg-1-6">μ = My</div>
+        <div className="pure-u-1-3 pure-u-sm-1-4 pure-u-md-1-5 pure-u-lg-1-6">ν = Ny</div>
+        <div className="pure-u-1-3 pure-u-sm-1-4 pure-u-md-1-5 pure-u-lg-1-6">ξ = Xi</div>
+        <div className="pure-u-1-3 pure-u-sm-1-4 pure-u-md-1-5 pure-u-lg-1-6">ο = Omikron</div>
+        <div className="pure-u-1-3 pure-u-sm-1-4 pure-u-md-1-5 pure-u-lg-1-6">π = Pi</div>
+        <div className="pure-u-1-3 pure-u-sm-1-4 pure-u-md-1-5 pure-u-lg-1-6">ρ = Rho</div>
+        <div className="pure-u-1-3 pure-u-sm-1-4 pure-u-md-1-5 pure-u-lg-1-6">σ = Sigma</div>
+        <div className="pure-u-1-3 pure-u-sm-1-4 pure-u-md-1-5 pure-u-lg-1-6">τ = Tau</div>
+        <div className="pure-u-1-3 pure-u-sm-1-4 pure-u-md-1-5 pure-u-lg-1-6">υ = Ypsilon</div>
+        <div className="pure-u-1-3 pure-u-sm-1-4 pure-u-md-1-5 pure-u-lg-1-6">φ = Phi</div>
+        <div className="pure-u-1-3 pure-u-sm-1-4 pure-u-md-1-5 pure-u-lg-1-6">χ = Chi</div>
+        <div className="pure-u-1-3 pure-u-sm-1-4 pure-u-md-1-5 pure-u-lg-1-6">ψ = Psi</div>
+        <div className="pure-u-1-3 pure-u-sm-1-4 pure-u-md-1-5 pure-u-lg-1-6">ω = Omega</div>
+      </div>
+    </div>
+    <div id="notesUnits" className="pure-u-1 pure-u-lg-1-2">
+      <h4>Einheiten:</h4>
+      <p>
+        <i>Lj</i> = <a href="https://de.wikipedia.org/wiki/Lichtjahr" target="_blank" title="Lichtjahr">Entfernung Erde-Stern in Lichtjahren</a>
+      </p>
+      <p>
+        <i>mag</i> = <a href="https://de.wikipedia.org/wiki/Scheinbare_Helligkeit" target="_blank" title="scheinbare Helligkeit">scheinbare Helligkeit</a> in mag
+      </p>
+      <p>
+        <i>M<sub>&#9737;</sub></i> = <a href="https://de.wikipedia.org/wiki/Sonnenmasse" target="_blank" title="Sonnenmassen">Sonnenmassen</a>
+      </p>
+      <p>
+        <i>R<sub>&#9737;</sub></i> = <a href="https://de.wikipedia.org/wiki/Sonnenradius" target="_blank" title="Sonnenradien">Sonnenradien</a>
+      </p>
+    </div>
   </div>
 );
 
