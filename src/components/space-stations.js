dc1302551907ea88cedde956c6bca9896a1e931b
cosmowiki <dev@cosmowiki.org>
2017-10-29 13:44:07 +0100

    edited the summary

--- src/components/space-stations.js
+++ src/components/space-stations.js
@@ -74,9 +74,9 @@ const SpaceStationsComponent = ({spaceStations:stations}) => {
         </p>
         <p className="summary-text">
           Eine der wichtigsten Erkenntnisse aus Skylab war die Notwendigkeit, eine
-          Raumstation mit Treibstoff auf auf einfache Weise wiederzubefüllen.
-          Durch die in einer Höhe von 300 bis 400 Kilometern zwar sehr dünne, aber
-          immer noch existierende Erdatmosphäre wird das Raumfahrzeug so stark
+          Raumstation mit Treibstoff auf einfache Weise wiederzubefüllen.
+          Auch in einer Höhe von 300 bis 400 Kilometern wird das Raumfahrzeug durch
+          die zwar sehr dünne, aber immer noch existierende Erdatmosphäre so stark
           gebremst, dass die Umlaufbahn in gewissen zeitlichen Abständen angehoben
           werden muss um einen Absturz zu verhindern.
         </p>
