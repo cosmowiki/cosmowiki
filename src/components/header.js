fd7e67196488c54c26b85e0ba5b87343a777c722
cosmowiki <dev@cosmowiki.org>
2017-11-06 00:16:36 +0100

    put a#navToggle in a div#navToggle
    The logo.js obsolete if the code is integrated into the header.js, but wait a while ;-)

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import Logo from './logo'
 import Navigation from './navigation'
 import Search from './search'
 
 const Header = ({appUrl}) => {
   return (
     <header className="pure-u-1">
-      <Logo/>
+      <div id="logo" className="pure-u-1-6 pure-u-md-1-4 pure-u-lg-1-6">
+        <a id="brand" href="/" title="CosmoWiki.de" rel="home"></a>
+      </div>
       <Navigation appUrl={appUrl} />
       <Search/>
+      <div id="navToggle" className="pure-u-1-6 pure-u-md-1-4 pure-u-lg-1-6 right">
+        <a href="#" title="Navigation"></a>
+      </div>
     </header>
   )
 };
--- src/components/logo.js
+++ src/components/logo.js
@@ -4,7 +4,9 @@ const Logo = () => {
   return (
     <div id="logo" className="pure-u-1-6 pure-u-md-1-4 pure-u-lg-1-6">
       <a id="brand" href="/" title="CosmoWiki.de" rel="home"></a>
-      <a id="navToggle" href="#"></a>
+    </div>
+    <div id="navToggle" className="pure-u-1-6 pure-u-md-1-4 pure-u-lg-1-6 right">
+      <a href="#" title="Navigation"></a>
     </div>
   );
 };
