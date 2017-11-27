12fae415b9b45bb2b1d946853da7e32dcb7ecdef
Travis-CI <w+travis-for-cosmowiki@kriesing.de>
2017-11-27 20:37:51 +0000

    Travis deployed 'master' - Mon Nov 27 20:37:50 UTC 2017

--- index.js
+++ index.js
@@ -37308,15 +37308,7 @@ Object.defineProperty(exports, '__esModule', {
   value: true
 });
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-
-var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var _react = require('react');
 
@@ -37350,12 +37342,6 @@ var PageComponent = function PageComponent(_ref) {
         ),
         _react2['default'].createElement(
           'li',
-          { id: 'toggleShariff' },
-          _react2['default'].createElement('a', { id: 'toggleShariffLink', href: '#' }),
-          _react2['default'].createElement(ShareButtons, null)
-        ),
-        _react2['default'].createElement(
-          'li',
           { id: 'scrollDown' },
           _react2['default'].createElement('a', { href: 'javascript:self.scrollTo(0,document.body.scrollHeight);' })
         )
@@ -37368,34 +37354,6 @@ var PageComponent = function PageComponent(_ref) {
 };
 
 exports['default'] = PageComponent;
-
-var ShareButtons = (function (_React$Component) {
-  _inherits(ShareButtons, _React$Component);
-
-  function ShareButtons() {
-    _classCallCheck(this, ShareButtons);
-
-    _get(Object.getPrototypeOf(ShareButtons.prototype), 'constructor', this).apply(this, arguments);
-  }
-
-  _createClass(ShareButtons, [{
-    key: 'shouldComponentUpdate',
-    value: function shouldComponentUpdate() {
-      return false; // we want shariff to control this component, so dont react to updates ;) (fingers crossed)
-    }
-  }, {
-    key: 'render',
-    value: function render() {
-      return _react2['default'].createElement('div', { className: 'shariff pure-u-1',
-        'data-services': 'twitter, facebook, googleplus, info',
-        'data-twitter-via': 'cosmowiki'
-      });
-    }
-  }]);
-
-  return ShareButtons;
-})(_react2['default'].Component);
-
 module.exports = exports['default'];
 
 },{"./footer":434,"./header":435,"react":454}],441:[function(require,module,exports){
