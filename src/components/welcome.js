2e49837ff66592c02239c39faa7a1b63f9818215
subnoise <dev@sub-noise.com>
2017-12-09 07:02:14 +0100

    welcome page finished so far

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -30,8 +30,13 @@ const WelcomeComponent = ({appUrl}) => {
 							Ringe des Saturn deutete er übrigens als Henkel des Planeten ;-)
 						</p>
 						<p>
-							Weitere <code>[numberOfEvents]-1</code> Ereignisse aus der spannenden Geschichte
-							von Astronomie und Raumfahrt finden Sie <a href="/events">hier</a>.
+							Wie alt ist die Himmelsscheibe von Nebra? Wann und wo wurden die ersten
+							Raketen gestartet? Was geschah am 30. Juni 1908 in Sibirien, und wann
+							startete das letzte Space-Shuttle?
+						</p>
+						<p>
+							<code>[numberOfEvents]</code> spannende Ereignisse aus der Geschichte
+							von Astronomie und Raumfahrt finden Sie <a href="/chronicle">hier</a>.
 						</p>
 					</div>
 		 	 	</div>
@@ -47,6 +52,11 @@ const WelcomeComponent = ({appUrl}) => {
 							als vielleicht älteste Wissenschaft.
 						</p>
 						<p>
+							Wer war Aryabhata und wo lebte er? Wofür legten Johannes Müller und
+							Nikolaus von Kues im 15. Jahrhundert die Grundsteine? Welche Erkenntnisse
+							verdanken wir Ole Rømer, und welche Edwin Hubble?
+						</p>
+						<p>
 							Weitere <code>[countScientists]-[countAstronomers]</code> Astrophysiker,
 							Kosmologen, Mathematiker und andere Wissenschaftler finden
 							Sie <a href="/scientists">hier</a>.
@@ -58,17 +68,18 @@ const WelcomeComponent = ({appUrl}) => {
 					<div className="welcome-text pure-u-1">
 						<p>
 							Eine unglaubliche Zahl! So viele Stunden haben alle <code>[countAstronauts]</code>
-							Astronauten zusammengerechnet bis heute<sup>*</sup> im All verbracht.
+							Astronauten zusammengerechnet bis heute im All verbracht.
 							Sie reisen in hauchdünnen Röhren aus Aluminium auf hunderten Tonnen
 							hochentzündlicher Treibstoffe mit mehreren Kilometern pro Sekunde
-							ins All. Dort erforschen sie neue Materialien und Fertigungsmethoden,
-							führen Installationen und Reparaturen an Raumschiffen durch und
-							setzen ihre Fußabdrücke auf fremde Himmelskörper. Sie sind die Botschafter
-							der Menschheit im Universum.
+							ins All. Dort leben und arbeiten sie, manchmal monatelang.
 						</p>
 						<p>
-							Besuchen Sie unsere Seite über <a href="/astronauts">Raumfahrer</a>.<br />
-							<sup>*</sup> <small>Stand: <code>[lastUpdated]</code></small>
+							Wer sind diese mutigen Frauen und Männer? An welcher Mission nahm Neil Armstrong vor Apollo 11 teil?
+							Welcher Astronaut absolvierte die längste Zeit im All, welcher die
+							meisten Außenbordeinsätze?
+						</p>
+						<p>
+							Auf unserer Seite über <a href="/astronauts">Raumfahrer</a> finden Sie die Antworten.<br />
 						</p>
 					</div>
 				</div>
@@ -80,10 +91,12 @@ const WelcomeComponent = ({appUrl}) => {
 							Neil Armstrong, Buzz Aldrin und Michael Collins am 16. Juli 1969 mit
 							Apollo 11 zu ihrem legendären Flug zum Mond. Doch hier begannen nicht
 							nur sämtliche Apollo-Missionen, auch alle 135 Space-Shuttle-Flüge
-							zwischen 1981 und 2011 starteten hier. LC-39 mit seinen zwei Plattformen
-							A und B ist der einzige Startplatz des Kennedy Space Center. Am Cape
-							Canaveral befinden sich noch über 30 weitere, meist inaktive Startplätze,
-							die allerdings zur Cape Canaveral Air Force Station (CCAFS) gehören.
+							zwischen 1981 und 2011 starteten hier.
+						</p>
+						<p>
+						 	Wo auf der Welt gibt es weitere Raketenstartplätze? Welche Forschungseinrichtungen
+							gehören zur ESA? Was verbirgt sich hinter dem Namen Rasad-e Khan,
+							und was war Uraniborg?
 						</p>
 						<p>
 							Erkunden Sie <code>[countPlaces]</code> interessante <a href="/places">Orte</a> der
@@ -100,8 +113,11 @@ const WelcomeComponent = ({appUrl}) => {
 							wurde von seinem Entdecker Wilhelm Herschel im Jahr 1781 nach dem
 							griechischen Himmelsgott Uranos benannt. Sein Sohn John Herschel gab
 							zwei Uranus-Monden die Namen von Figuren aus Shakespeares Sommernachtstraum
-							- Titania und Oberon. Vom Saturn zum Beispiel sind bislang <code>[countSaturnMoons]</code> Monde
-							bekannt, von Jupiter sogar <code>[countJupiterMoons]</code>.
+							- Titania und Oberon.
+						</p>
+						<p>
+							Wieviele Monde umkreisen den Saturn, wieviele den Jupiter? Was sind
+							Zentauren und wo befindet sich die Oortsche Wolke?
 						</p>
 						<p>
 							Entdecken Sie <code>[countSolsysBodies]</code> Himmelskörper unseres <a href="/solar-system">Sonnensystems</a>.
@@ -111,64 +127,108 @@ const WelcomeComponent = ({appUrl}) => {
 				<div id="welcomeConstellations" className="welcome-container">
 					<h4>Wegweiser am Nachthimmel</h4>
 					<div className="welcome-text pure-u-1">
-						<p>Wie findet man den Polarstern? Ganz einfach, mit Hilfe von Sternbildern:
-						Fünfmal wird die Strecke zwischen den beiden hinteren Sternen des Großen
-						Wagens, der gar kein offizielles Sternbild ist, nach oben verlängert.
-						Gleichzeitig ist der Polarstern auch der letzte an der Deichsel des
-						Kleinen Wagens, und auch die Kassiopeia - das Himmels-W - zeigt auf ihn.
-						So wiesen die immer gleichen Bilder den Seeleuten und Karawanen schon
-						vor Jahrhunderten den Weg, und das alljährliche Erscheinen und Verschwinden
-						von Sternbildern wurde bestimmend - nicht nur für die Landwirtschaft.</p>
-						<p>Im Jahr 1922 wurden 88 Konstellationen von der IAU
-						definiert. <a href="/constellations">Hier</a> finden Sie alle wichtigen
-						Informationen über diese Sternbilder.</p>
+						<p>
+							Fünfmal wird die Strecke zwischen den beiden hinteren Sternen des
+							Großen Wagens nach oben verlängert, und wir sehen den Polarstern,
+							der gleichzeitig auch der letzte an der Deichsel des Kleinen Wagens
+							ist. Auch die Kassiopeia - das Himmels-W - zeigt auf ihn.
+						</p>
+						<p>
+							Welches ist das kleinste aller Sternbilder? Warum trägt eines den
+							merkwürdigen Namen "Luftpumpe"? Welcher Stern ist der hellste im Orion,
+							und welche Sternbilder sind noch am Südpol zu sehen?
+						</p>
+						<p>
+							<a href="/constellations">Hier</a> finden Sie die Antworten und viele
+							weitere Informationen zu allen 88 Sternbildern.
+						</p>
 		 	 		</div>
 		 	 	</div>
 				<div id="welcomeStars" className="welcome-container">
 					<h4>70.000.000.000.000.000.000.000!</h4>
 					<div className="welcome-text pure-u-1">
-						<p>Die Zahl aller Sterne im Universum wird auf etwa 7 &times; 10<sup>22</sup> geschätzt.
-						70 Trilliarden Sterne in 100 Milliarden Galaxien - einfach unglaublich!
-						Hier auf CosmoWiki.org finden Sie natürlich nicht alle dieser Sonnen, aber
-						zu immerhin <code>[countStars]</code> von ihnen, die wichtigsten Angaben,
-						wie zum Beispiel ihre Entfernung zu uns, ihre Größe, Masse, Leuchtkraft
-						und viele weitere Daten.</p>
-						<p>Wir holen Ihnen die Sterne vom Himmel - <a href="/stars">hier</a> finden Sie sie.</p>
+						<p>
+							Die Zahl aller Sterne im Universum wird auf etwa 7 &times; 10<sup>22</sup> geschätzt.
+							70 Trilliarden Sterne in 100 Milliarden Galaxien - einfach unglaublich!
+							Hier auf CosmoWiki.org finden Sie natürlich nicht alle dieser Sonnen, aber
+							immerhin <code>[countStars]</code> von ihnen, die wichtigsten Angaben,
+							wie zum Beispiel ihre Entfernung zu uns, ihre Größe, Masse, Leuchtkraft
+							und viele weitere Daten.
+						</p>
+						<p>
+							Wie weit entfernt von uns ist der Polarstern? Aus wievielen einzelnen
+							Sternen besteht der Zwilling Castor? In welchem Sternbild ist Sirius
+							zu finden, und welcher Stern verbirgt sich hinter α Lyrae?
+						</p>
+						<p>
+							Wir holen Ihnen die Sterne vom Himmel - <a href="/stars">hier</a> finden Sie sie.
+						</p>
 		 	 		</div>
 		 	 	</div>
 				<div id="welcomeSpacestations" className="welcome-container">
 					<h4>Hätten Sie gedacht, ...</h4>
 					<div className="welcome-text pure-u-1">
-						<p>... dass die Sowjets in den 1970er Jahren bereits sieben Weltraumstationen
-						erfolgreich gestartet hatten? Fünf davon wurden sogar bemannt und lieferten
-						wichtige Erkenntnisse über die Auswirkungen längerer Aufenthalte von
-						Menschen im All.</p>
-						<p>Heute ist das Leben auf Weltraumstationen scheinbar zur Routine
-						geworden, doch tatsächlich ist es nur eine sehr kleine Elite, die erst
-						nach mehrjährigen Studiengängen und jahrelangem Training Plätze auf den
-						aktuellen Stationen im Orbit belegen darf.</p>
-						<p>Erkunden Sie hier bei uns vergangene und künftige <a href="/space-stations">Weltraumstationen</a>!</p>
+						<p>
+							... dass die Sowjets in den 1970er Jahren bereits sieben Weltraumstationen
+							erfolgreich gestartet hatten? Fünf davon wurden sogar bemannt und
+							lieferten wichtige Erkenntnisse über die Auswirkungen längerer Aufenthalte
+							von Menschen im All. Heute ist das Leben auf Weltraumstationen scheinbar
+							zur Routine geworden, doch tatsächlich ist es nur eine sehr kleine Elite,
+							die erst nach mehrjährigen Studiengängen und jahrelangem Training
+							Plätze auf den aktuellen Stationen im Orbit belegen darf.
+						</p>
+						<p>
+							Wie sahen die ersten sowjetischen Stationen aus? Wieviele Besatzungen
+							besuchten das Skylab? Wie lange umkreiste die Raumstation Mir die Erde
+							und wann wird die nächste chinesische Station gestartet?
+						</p>
+						<p>
+							Erkunden Sie hier bei uns vergangene und künftige <a href="/space-stations">Weltraumstationen</a>!
+						</p>
 		 	 		</div>
 		 	 	</div>
 				<div id="welcomeSpacetelescopes" className="welcome-container">
 					<h4>Alles schwingt.</h4>
 					<div className="welcome-text pure-u-1">
-						<p>Von Gammastrahlen bis Radiowellen, über Röntgenstrahlung, ultraviolettes,
-						sichtbares und infrarotes Licht und Mikrowellen erstreckt sich der Bereich
-						des elektromagnetischen Spektrums, aus dem Wissenschaftler rund um den
-						Globus immer wieder neue Erkenntnisse über den Aufbau und die Entstehung
-						des Universums gewinnen. Enorm viele Daten liefern ihnen dabei Weltraumteleskope,
-						die in Zukunft auch Gravitationswellen detektieren werden.
+						<p>
+							Von Gammastrahlen bis Radiowellen, über Röntgenstrahlung, ultraviolettes,
+							sichtbares und infrarotes Licht und Mikrowellen erstreckt sich der Bereich
+							des elektromagnetischen Spektrums, aus dem Wissenschaftler rund um den
+							Globus immer wieder neue Erkenntnisse über den Aufbau und die Entstehung
+							des Universums gewinnen. Enorm viele Daten liefern ihnen dabei Weltraumteleskope,
+							die in Zukunft auch Gravitationswellen detektieren werden.
 						</p>
-						<p>Ausführliche Informationen zu <code>[countSpacetelescopes]</code> Augen
-						und Ohren im All gibt es <a href="/space-telescopes">hier</a>.</p>
+						<p>
+							Welches Land spezialisierte sich schon sehr früh auf Röntgenteleskope
+							im All? In welchen Wellenlängen arbeitet Hubble und wann wird sein
+							Nachfolger, das James-Webb-Weltraumteleskop starten?
+						</p>
+						<p>
+							Ausführliche Informationen zu <code>[countSpacetelescopes]</code> Augen
+							und Ohren im All gibt es <a href="/space-telescopes">hier</a>.
+						</p>
 		 	 		</div>
 		 	 	</div>
 				<div id="welcomeMissions" className="welcome-container">
-					<h4></h4>
+					<h4>Einmal ins Weltall und wieder zurück, bitte.</h4>
 					<div className="welcome-text pure-u-1">
-						<p></p>
-						<p><a href="/missions">Missionen</a></p>
+						<p>
+							Mit dem Start von Sputnik 1 am 4. Oktober 1957 begann das Zeitalter
+							der Raumfahrt. Von diesem Tag an wurden Tausende Satelliten, Transportraumschiffe,
+							Sonden, bemannte Raumkapseln, Module für Weltraumstationen und Weltraumteleskope
+							in den Erdorbit, zu anderen Planeten oder sogar an das Ende unseres
+							Sonnensystems befördert.
+						</p>
+						<p>
+							Von wo startete Voyager 1? Welches Land erkundete als erstes die
+							Venus mit einer Raumsonde? Wann kehrten die Astronauten von Apollo 13 zurück, und wie hieß der
+							erste indische Satellit?
+						</p>
+						<p>
+							Hier finden Sie umfassende Informationen
+							über <a href="/missions"><code>[countMissions]</code> spannende Missionen</a>,
+							ihre Ziele und Ergebnisse. Sie werden begeistert sein!
+						</p>
 		 	 		</div>
 		 	 	</div>
 			</div>
