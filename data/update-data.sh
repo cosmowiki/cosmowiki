c562a2898c74f8a3f661f9a81a471eafff7f8a8e
subnoise <dev@sub-noise.com>
2018-03-09 03:12:09 +0100

    jsons updated
    new observatories.json,
    new launchpads.json,
    new groundstations.json

--- data/update-data.sh
+++ data/update-data.sh
@@ -44,44 +44,62 @@ if [[ $# == 0 || $1 == places ]]; then
   echo "`du -h $FILENAME` loaded"
 fi
 
+if [[ $# == 0 || $1 == groundstations ]]; then
+  FILENAME=data/groundstations.json
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 7
+  echo "`du -h $FILENAME` loaded"
+fi
+
+if [[ $# == 0 || $1 == observatories ]]; then
+  FILENAME=data/observatories.json
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 8
+  echo "`du -h $FILENAME` loaded"
+fi
+
+if [[ $# == 0 || $1 == launchpads ]]; then
+  FILENAME=data/launchpads.json
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 9
+  echo "`du -h $FILENAME` loaded"
+fi
+
 if [[ $# == 0 || $1 == missions ]]; then
   FILENAME=data/missions.json
-  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 7
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 10
   echo "`du -h $FILENAME` loaded"
 fi
 
 if [[ $# == 0 || $1 == stars ]]; then
   FILENAME=data/stars.json
-  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 8
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 11
   echo "`du -h $FILENAME` loaded"
 fi
 
 if [[ $# == 0 || $1 == constellations ]]; then
   FILENAME=data/constellations.json
-  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 9
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 12
   echo "`du -h $FILENAME` loaded"
 fi
 
 if [[ $# == 0 || $1 == spacestations ]]; then
   FILENAME=data/spacestations.json
-  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 10
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 13
   echo "`du -h $FILENAME` loaded"
 fi
 
 if [[ $# == 0 || $1 == spacetelescopes ]]; then
   FILENAME=data/spacetelescopes.json
-  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 11
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 14
   echo "`du -h $FILENAME` loaded"
 fi
 
 if [[ $# == 0 || $1 == solarsystem ]]; then
   FILENAME=data/solarsystem.json
-  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 12
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 15
   echo "`du -h $FILENAME` loaded"
 fi
 
 if [[ $# == 0 || $1 == spacewalks ]]; then
   FILENAME=data/spacewalks.json
-  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 13
+  gsjson 1YY06xH0Q6AWq8W1FLcY_h9dWl9Q7ECFOu6J6qbXKgMM --beautify $FILENAME --worksheet 16
   echo "`du -h $FILENAME` loaded"
 fi
