d6b735722b856425c8011221d6e604906b537565
subnoise <dev@sub-noise.com>
2018-03-30 07:47:43 +0200

    bla

--- src/components/constellations.js
+++ src/components/constellations.js
@@ -188,7 +188,7 @@ const ConstellationComponent = ({constellation:item}) => {
         <div className="constellation-data pure-u-1 pure-u-md-1-2">
           <p className="constellation-data1">sichtbar von {item.visibleFrom} bis {item.visibleTo}</p>
           <p className="constellation-data2">Fläche: {item.squareDegrees} deg²</p>
-          <p className="constellation-data3">hellster Stern:<br />{item.brightestStarUrl ? <a href={item.brightestStarUrl} title={item.brightestStar}>{item.brightestStar}</a> : `${item.brightestStar}`} ({item.highestBrightness} mag)</p>
+          <p className="constellation-data3">hellster Stern:<br />{item.brightestStarUrl ? <a href={item.brightestStarUrl} title={item.brightestStar}>{item.brightestStar}</a> : item.brightestStar} ({item.highestBrightness} mag)</p>
           <p className="constellation-data4">benannt {item.namedYear} von <a href={item.astronomerUrl} title={item.astronomer}>{item.astronomer}</a></p>
         </div>
       </div>
